dfbc5e764398a6e078676a3b8bb418c23943cd17
eamon <devnull@localhost>
2008-02-20 20:40:06 +0000

    "Undeleted" EamonExtensionsLinq from eamon-docs-trunk

---
+++ EmnExtensions/Text/StringFeatures.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EamonExtensionsLinq.Text
+{
+	public static class StringFeatures
+	{
+		public static bool IsNullOrEmpty(this string str) {
+			return str == null || str.Length == 0;
+		}
+
+	}
+}
