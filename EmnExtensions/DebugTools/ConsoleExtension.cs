e058e0eb28c166c3296f95d17150456b3e4bb47e
eamon <devnull@localhost>
2008-02-21 13:00:29 +0000

    Loaded EamonExtensionsLinq from before copy to eamonpublic repository.

---
+++ EmnExtensions/DebugTools/ConsoleExtension.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EamonExtensionsLinq.DebugTools {
+    public static class ConsoleExtension {
+        public static void PrintAllDebug<T>(this IEnumerable<T> source) {
+            foreach (var item in source)
+                Console.WriteLine(item);
+        }
+
+        public static T PrintDebug<T>(this T obj) {
+            Console.WriteLine(obj);
+            Console.ReadKey();
+            return obj;
+        }
+    }
+}
---
+++ EmnExtensions/Text/TryParse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EamonExtensionsLinq.Text
+{
+	public static class ParseString
+	{
+		public static DateTime? ParseAsDateTime(this string s) {
+			DateTime val;
+					if (DateTime.TryParse(s, out val)) return val;
+					else return null;
+		}
+		public static int? ParseAsInt32(this string s) {
+			int val;
+			if(int.TryParse(s, out val)) return val;
+			else return null;
+		}
+
+		public static ulong? ParseAsUInt64(this string s) {
+			ulong val;
+			if(ulong.TryParse(s, out val)) return val;
+			else return null;
+		}
+
+		public static long? ParseAsInt64(this string s) {
+			long val;
+			if(long.TryParse(s, out val)) return val;
+			else return null;
+		}
+	}
+}
