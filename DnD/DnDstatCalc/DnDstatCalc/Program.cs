b39044e0d740aa90fcd69ca2e40f635ebd944699
eamon <devnull@localhost>
2008-06-30 15:01:00 +0000

    Updated stat calc to actually calculate stat values.

--- DnD/DnDstatCalc/DnDstatCalc/Program.cs
+++ DnD/DnDstatCalc/DnDstatCalc/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-
+using EamonExtensionsLinq.Algorithms;
 namespace DnDstatCalc {
     public struct Prob<T> {
         public T Val;
@@ -61,21 +61,50 @@ namespace DnDstatCalc {
             };
 
             double sumcosts=0.0;
-            int countcosts = 0;
-            foreach (var x in Enumerable.Range(0, 100000000)) {
-                var stats = new[] { rollStat(), rollStat(), rollStat(), rollStat(), rollStat(), rollStat() }.OrderBy(stat=>stat).ToArray();
+            int count = 0;
+			int reach15 = 0;
+			int reach14h = 0;
+			int cost22 = 0;
+			int cost22plus = 0;
+            foreach (var x in Enumerable.Range(0, 10000000)) {
+				var stats = new[] { rollStat(), rollStat(), rollStat(), rollStat(), rollStat(), rollStat() }.OrderBy(i=>i).ToArray();
                 stats[0] = rollStat();
-                //ok, we rerolled the lowest.
+				//ok, we rerolled the lowest.
+				//now find weighted average stat value:
+				stats.Shuffle();
+				var indexed= stats.Select((stat, idx) => new { Index = idx, Val = stat });
+				var groupedStats = (from idxStat in indexed
+				 group idxStat by idxStat.Index/2 into defenseGroup
+				 let betterStat = defenseGroup.Select(s=>s.Val).Max()
+                 let worseStat = defenseGroup.Select(s=>s.Val).Min()
+				 select new {Good=betterStat,Bad=worseStat}).ToArray();
+				var attackStat = groupedStats.First().Good;
+				var defStatSum = groupedStats.Select(s => s.Good).Sum();
+				var secondaryStat = stats.OrderBy(s => s).Skip(1).First();
+				var offStatSum = groupedStats.Select(s => s.Bad).Sum();
+				var weightedAvg = (3 * attackStat + defStatSum + secondaryStat + offStatSum / 3.0) / 8.0; //could be (3*18 + (18+14+11)+14+(10+10+8)/3.0)/8.0
+				//so pointbuy allows 361/24== 15+1/24),though def. is 16,14,13,12,11,10
+				//def is: 4*16+2*14 + 13 + 11
 
                 var totalmod = stats.Select(stat => StatMod(stat)).Sum();
                 var totalcost = stats.Select(stat => StatVal(stat)).Sum()+2;
                 //if(totalcost>22)Console.WriteLine(string.Join(", ", stats.Select(i => i.ToString()).ToArray()) + ":  " + totalmod + "   ("+totalcost+")");
                 if (totalmod < 4 || totalmod > 8) continue;
-                sumcosts += totalcost;
-                countcosts++;
+
+				if (weightedAvg > 15.04166) reach15++;
+				if (weightedAvg >= 14.5) reach14h++;
+				if (totalcost > 22) cost22plus++;
+				if (totalcost >= 22) cost22++;
+				sumcosts += totalcost;
+                count++;
             }
-            Console.WriteLine("Avg:" + sumcosts / countcosts);
-            Console.ReadLine();
+            Console.WriteLine("Avg:" + sumcosts / count);
+			Console.WriteLine("ReachDefaultArray: " + reach14h*100.0 / count + "%");
+			Console.WriteLine("ReachOptimum: " + reach15 * 100.0 / count + "%");
+			Console.WriteLine("ReachPointBuy: " + cost22 * 100.0 / count + "%");
+			Console.WriteLine("ExceedPointBuy: " + cost22plus * 100.0 / count + "%");
+
+			//Console.ReadLine();
         }
     }
 }
