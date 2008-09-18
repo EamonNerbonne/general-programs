7ae515faa8901604bfa37c6751ee2e08e7747ded
marten <devnull@localhost>
2008-09-18 12:35:06 +0000

    Support for inserting and updating trackinfo. Deletion of trackinfo is only through deletion of Track (or Track.Artist) itself.
    TODO: Think of a good way of retreiving trackinfo. Preferably through Track, but may need to lazily load for performance reasons?

---
+++ SongSearchLinq/LastFMspider/LastFMSQLiteBackend/InsertTrackInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+
+namespace LastFMspider.LastFMSQLiteBackend
+{
+    public class InsertTrackInfo : AbstractLfmCacheQuery
+    {
+        public InsertTrackInfo(LastFMSQLiteCache lfm)
+            : base(lfm)
+        {
+            lowerArtist = DefineParameter("@lowerArtist");
+            lowerTrack = DefineParameter("@lowerTrack");
+            listeners = DefineParameter("@listeners");
+            playcount = DefineParameter("@playcount");
+            duration = DefineParameter("@duration");
+            artistMbid = DefineParameter("@lowerArtistMbid");
+            trackMbid = DefineParameter("@lowerTrackMbid");
+            lastFmId = DefineParameter("@lastFmId");
+        }
+
+        protected override string CommandText
+        {
+            get
+            {
+                return @"
+INSERT OR IGNORE INTO [TrackInfo] (TrackID, Listeners, Playcount, Duration, ArtistMbidID, TrackMbidID, LastFmId)
+SELECT TR.TrackID, @listeners, @playcount, @duration, A.MbidID, T.MbidID, @lastFmId FROM Artist AR, Track TR, Mbid A, Mbid T
+WHERE AR.LowercaseArtist = @lowerArtist AND TR.ArtistID = AR.ArtistID 
+  AND A.LowercaseMbid = @lowerArtistMbid 
+  AND T.LowercaseMbid = @lowerTrackMbid
+";
+            }
+        }
+        DbParameter lowerArtist, lowerTrack, listeners, playcount, duration, artistMbid, trackMbid, lastFmId;
+
+        public void Execute(SongRef songref)
+        {
+            lowerTrack.Value = songref.Title.ToLowerInvariant();
+            lowerArtist.Value = songref.Artist.ToLowerInvariant();
+            CommandObj.ExecuteNonQuery();
+        }
+
+    }
+}
---
+++ SongSearchLinq/LastFMspider/LastFMSQLiteBackend/UpdateTrackInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Common;
+using System;
+namespace LastFMspider.LastFMSQLiteBackend
+{
+    public class UpdateTrackInfo : AbstractLfmCacheQuery
+    {
+        public UpdateTrackInfo(LastFMSQLiteCache lfm)
+            : base(lfm)
+        {
+            id = DefineParameter("@id");
+            listeners = DefineParameter("@listeners");
+            playcount = DefineParameter("@playcount");
+            duration = DefineParameter("@duration");
+            artistMbid = DefineParameter("@lowerArtistMbid");
+            trackMbid = DefineParameter("@lowerTrackMbid");
+            lastFmId = DefineParameter("@lastFmId");
+            timestamp = DefineParameter("@timestamp");
+        }
+
+        protected override string CommandText
+        {
+            get { return @"UPDATE TrackInfo   SET Listeners=@listeners, InfoTimestamp=@timestamp, 
+                                                  Playcount=@playcount, Duration=@duration,
+                                                  ArtistMbidID=(SELECT MbidID FROM Mbid WHERE LowercaseMbid = @lowerArtistMbid), 
+                                                  TrackMbidID=(SELECT MbidID FROM Mbid WHERE LowercaseMbid = @lowerTrackMbid),
+                                                  LastFmId=@lastFmId
+                                            WHERE TrackID=@id"; }
+        }
+
+        DbParameter id, listeners, playcount, duration, artistMbid, trackMbid, lastFmId, timestamp;
+
+        public void Execute(TrackRow row, TrackInfoRow infoRow)
+        {
+            id.Value = row.TrackID;
+            timestamp.Value = infoRow.InfoTimestamp == null ? DBNull.Value : (object)infoRow.InfoTimestamp.Value.Ticks;
+
+            listeners.Value = infoRow.Listeners;
+            playcount.Value = infoRow.Playcount;
+            duration.Value = infoRow.Duration;
+            artistMbid.Value = infoRow.ArtistMbidID
+            CommandObj.ExecuteNonQuery();
+        }
+    }
+}
