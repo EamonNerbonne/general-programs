d1626dded740a6d8f93f25057cdadb850f5e43ba
eamon <devnull@localhost>
2008-09-18 15:05:52 +0000

    Initial version of WS processing.

---
+++ SongSearchLinq/LastFMspider/LfmApi/TrackGetSimilar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace LastFMspider.LfmApi
+{
+    public class LfmArtist
+    {
+        public string name;
+        public string mbid;
+    }
+
+    public class LfmSimilarTrack
+    {
+        public string name;
+        public float match;
+        public LfmArtist artist;
+    }
+
+    public class TrackGetSimilar
+    {
+        [XmlElement("track")]
+        public LfmSimilarTrack[] track;
+        
+        [XmlAttribute("track")]  public string trackTitle;
+        [XmlAttribute]   public string artist;
+    }
+    [XmlRoot("lfm")]
+    public class LfmStatus
+    {
+        public TrackGetSimilar similartracks;
+        [XmlAttribute]  public string status;
+    }
+}
---
+++ SongSearchLinq/LfmApiSerializationTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EamonExtensionsLinq;
+using EamonExtensionsLinq.Filesystem;
+using System.IO;
+using System.Xml.Serialization;
+using LastFMspider.LfmApi;
+using System.Xml;
+namespace LfmApiSerializationTest
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            FileInfo getsimilarFI = new FileInfo(@"C:\Users\Eamon\eamonhome\docs-trunk\VS.NET2008\SongSpider\LastFMspider\track.getsimilar.xml");
+            FileInfo getinfoFI = new FileInfo(@"C:\Users\Eamon\eamonhome\docs-trunk\VS.NET2008\SongSpider\LastFMspider\track.getinfo.xml");
+            FileInfo gettoptagsFI = new FileInfo(@"C:\Users\Eamon\eamonhome\docs-trunk\VS.NET2008\SongSpider\LastFMspider\track.gettoptags.xml");
+            XmlSerializer serializer = new XmlSerializer(typeof(LfmStatus));
+            XmlDocument doc = new XmlDocument();
+            doc.Load(getsimilarFI.OpenRead());
+            object o= serializer.Deserialize(getsimilarFI.OpenRead());
+            serializer.Serialize(Console.Out, o);
+            Console.ReadKey();
+        }
+    }
+}
