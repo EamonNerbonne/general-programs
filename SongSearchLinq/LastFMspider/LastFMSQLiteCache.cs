9c00f79b33c442a95951dd2523b924e774e35964
eamon <devnull@localhost>
2008-09-18 09:13:39 +0000

    Added sql code for new tables.

--- SongSearchLinq/LastFMspider/LastFMSQLiteCache.cs
+++ SongSearchLinq/LastFMspider/LastFMSQLiteCache.cs
@@ -17,51 +17,109 @@ namespace LastFMspider {
         const string DataConnectionString = "page size=4096;cache size=262144;datetimeformat=Ticks;Legacy Format=False;Synchronous=N;data source=\"{0}\"";
         const string DatabaseDef = @"
 CREATE TABLE IF NOT EXISTS [Artist] (
-[ArtistID] INTEGER  PRIMARY KEY NOT NULL,
-[FullArtist] TEXT  NOT NULL,
-[LowercaseArtist] TEXT  NOT NULL
+  [ArtistID] INTEGER  PRIMARY KEY NOT NULL,
+  [FullArtist] TEXT  NOT NULL,
+  [LowercaseArtist] TEXT  NOT NULL
+);
+CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_Artist_LowercaseArtist] ON [Artist](
+  [LowercaseArtist]  ASC
 );
 
-CREATE TABLE IF NOT EXISTS [SimilarTrack] (
-[SimilarTrackID] INTEGER  NOT NULL PRIMARY KEY,
-[TrackA] INTEGER  NOT NULL,
-[TrackB] INTEGER  NOT NULL,
-[Rating] REAL NOT NULL
-);
+
 
 CREATE TABLE IF NOT EXISTS  [Track] (
-[TrackID] INTEGER  NOT NULL PRIMARY KEY,
-[ArtistID] INTEGER  NOT NULL,
-[FullTitle] TEXT  NOT NULL,
-[LowercaseTitle] TEXT  NOT NULL,
-[LookupTimestamp] INTEGER  NULL
+  [TrackID] INTEGER  NOT NULL PRIMARY KEY,
+  [ArtistID] INTEGER  NOT NULL,
+  [FullTitle] TEXT  NOT NULL,
+  [LowercaseTitle] TEXT  NOT NULL,
+  [LookupTimestamp] INTEGER  NULL
 );
-
-CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_Artist_LowercaseArtist] ON [Artist](
-[LowercaseArtist]  ASC
-);
-
-CREATE INDEX  IF NOT EXISTS [IDX_SimilarTrack_TrackB] ON [SimilarTrack](
-[TrackB]  ASC
-);
-
-CREATE INDEX  IF NOT EXISTS [IDX_SimilarTrack_Rating] ON [SimilarTrack](
-[Rating]  DESC
-);
-
-
-CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_SimilarTrack_TrackA_TrackB] ON [SimilarTrack](
-[TrackA]  ASC,
-[TrackB]  ASC
-);
-
 CREATE UNIQUE INDEX IF NOT EXISTS [Unique_Track_ArtistID_LowercaseTitle] ON [Track](
-[ArtistID]  ASC,
-[LowercaseTitle]  ASC
+  [ArtistID]  ASC,
+  [LowercaseTitle]  ASC
+);
+CREATE INDEX IF NOT EXISTS [IDX_Track_LookupTimestamp] ON [Track](
+  [LookupTimestamp]  ASC
 );
 
-CREATE INDEX IF NOT EXISTS [IDX_Track_LookupTimestamp] ON [Track](
-[LookupTimestamp]  ASC
+
+
+CREATE TABLE IF NOT EXISTS [SimilarTrack] (
+  [SimilarTrackID] INTEGER  NOT NULL PRIMARY KEY,
+  [TrackA] INTEGER  NOT NULL,
+  [TrackB] INTEGER  NOT NULL,
+  [Rating] REAL NOT NULL
+);
+CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_SimilarTrack_TrackA_TrackB] ON [SimilarTrack](
+  [TrackA]  ASC,
+  [TrackB]  ASC
+);
+CREATE INDEX  IF NOT EXISTS [IDX_SimilarTrack_TrackB] ON [SimilarTrack](
+  [TrackB]  ASC
+);
+CREATE INDEX  IF NOT EXISTS [IDX_SimilarTrack_Rating] ON [SimilarTrack](
+  [Rating]  DESC
+);
+
+
+
+CREATE TABLE IF NOT EXISTS  [Tag] (
+  [TagID] INTEGER NOT NULL PRIMARY KEY,
+  [LowercaseTag] TEXT  NOT NULL
+);
+CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_Tag_LowercaseTag] ON [Tag](
+  [LowercaseTag]  ASC
+);
+
+
+
+CREATE TABLE IF NOT EXISTS  [TrackTag] (
+  [TrackTagID] INTEGER NOT NULL PRIMARY KEY,
+  [TagID] INTEGER NOT NULL,
+  [TrackID] INTEGER NOT NULL,
+  [TagCount] INTEGER NOT NULL
+);
+CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_TrackTag_TrackID_TagID] ON [TrackTag](
+  [TrackID]  ASC,
+  [TagID] ASC
+);
+CREATE INDEX  IF NOT EXISTS [IDX_TrackTag_TagID] ON [TrackTag](
+  [TagID] ASC
+);
+CREATE INDEX  IF NOT EXISTS [IDX_TrackTag_TagCount] ON [TrackTag](
+  [TagCount] DESC
+);
+
+
+
+CREATE TABLE IF NOT EXISTS  [Mbid] (
+  [MbidID] INTEGER NOT NULL PRIMARY KEY,
+  [LowercaseMbid] TEXT NOT NULL
+);
+CREATE UNIQUE INDEX  IF NOT EXISTS [Unique_Mbid_LowercaseMbid] ON [Mbid](
+  [LowercaseMbid]  ASC
+);
+
+
+
+CREATE TABLE IF NOT EXISTS  [TrackInfo] (
+  [TrackID] INTEGER NOT NULL PRIMARY KEY,
+  [InfoTimestamp] INTEGER NOT NULL,
+  [Listeners] INTEGER NULL,
+  [Playcount] INTEGER NULL,
+  [Duration] INTEGER NULL,
+  [ArtistMbidID] INTEGER NULL,
+  [TrackMbidID] INTEGER NULL,
+  [LastFmId] INTEGER NULL
+);
+CREATE INDEX  IF NOT EXISTS [IDX_TrackInfo_InfoTimestamp] ON [TrackInfo](
+  [InfoTimestamp] ASC
+);
+CREATE INDEX  IF NOT EXISTS [IDX_TrackInfo_Listeners] ON [TrackInfo](
+  [Listeners] DESC
+);
+CREATE INDEX  IF NOT EXISTS [IDX_TrackInfo_Playcount] ON [TrackInfo](
+  [Playcount] DESC
 );
 ";
 
