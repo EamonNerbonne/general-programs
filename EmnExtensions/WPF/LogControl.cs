529612b6170ccc70ad600a360c65ff8d6ec93eb8
eamon <devnull@localhost>
2008-10-23 08:58:24 +0000

    Refactored HWR LogControl into EamonExtensionsLinq.csproj

---
+++ EmnExtensions/WPF/LogControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using EamonExtensionsLinq.Text;
+
+namespace EamonExtensionsLinq.WPF
+{
+    public class LogControl : TextBox
+    {
+
+        StringBuilder sb = new StringBuilder();
+        bool redraw = false;
+
+        public void AppendLineThreadSafe(string line) {
+            lock (sb) {
+                sb.AppendLine(line);
+                Invalidate();
+            }
+        }
+
+        private void Invalidate() {
+            if (!redraw) {
+                redraw = true;
+                Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(UpdateStringUI));
+            }
+        }
+        public void AppendThreadSafe(string text) {
+            lock (sb) {
+                sb.Append(text);
+                Invalidate();
+            }
+        }
+        private void UpdateStringUI() {
+            lock (sb) {
+                if (redraw) {
+                    redraw = false;
+                    Text += sb.ToString();
+                    sb.Length = 0;
+                    ScrollToEnd();
+                }
+            }
+        }
+        public void ClaimStandardOut() {
+            Console.SetOut(new DelegateTextWriter(AppendThreadSafe));
+        }
+    }
+}
