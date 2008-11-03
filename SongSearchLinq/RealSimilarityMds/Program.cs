1aa7405fbb79bbc81836a611d665139895ac71ff
eamon <devnull@localhost>
2008-11-03 21:59:41 +0000

    - Added actual mds to RealSimilarityMds. - Added lookup code which connects SongRef (via sqlite) to sqlite TrackId, (via SimilarTracks/TrackMapper) to denseId, (via CachedDistanceMatrix.Mapping) to mdsID - Added SongData usage to use billboard songs as test data.  SongData is mapped via the previous pipeline. -Renamed Main to Run in preparation for WPF app change

--- SongSearchLinq/RealSimilarityMds/Program.cs
+++ SongSearchLinq/RealSimilarityMds/Program.cs
@@ -7,6 +7,7 @@ using SongDataLib;
 using LastFMspider;
 using System.IO;
 using System.Text.RegularExpressions;
+using hitmds;
 
 namespace RealSimilarityMds
 {
@@ -15,25 +16,105 @@ namespace RealSimilarityMds
         static LastFmTools tools;
         static Regex fileNameRegex = new Regex(@"(e|t|b)(?<num>\d+)\.(dist|bin)", RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.ExplicitCapture);
 
-        static void Main(string[] args) {
-            NiceTimer timer = new NiceTimer();
-            timer.TimeMark("Loading");
+
+        static void Run() {
             SongDatabaseConfigFile config = new SongDatabaseConfigFile(false);
             tools = new LastFmTools(config);
-            //var sims = SimilarTracks.LoadOrCache(tools, SimilarTracks.DataSetType.Training);
-            //sims.ConvertToDistanceFormat();
-            timer.TimeMark("GC");
-            System.GC.Collect();
+
+            
+            CachedDistanceMatrix cachedMatrix = LoadCachedMatrix(tools);
+            Console.WriteLine("dists: {0} total, {1} non-finite", cachedMatrix.Matrix.Values.Count(), cachedMatrix.Matrix.Values.Where(f => f.IsFinite()).Count());
+            
+            var positionedPoints = DoMds(cachedMatrix);
+
+
+            Dictionary<int,SongRef> songrefByMds= WellKnownTracksByMdsId(tools,cachedMatrix);
+            
+
+            FileInfo logFile = new FileInfo (Path.Combine (tools.ConfigFile.DataDirectory.FullName,@".\mdsdPoints.txt") ); 
+
+            using(Stream s = logFile.Open(FileMode.Create, FileAccess.Write))
+            using (TextWriter writer = new StreamWriter(s)) {
+                for (int i = 0; i < positionedPoints.GetLength(0); i++) {
+                    if (songrefByMds.ContainsKey(i)) {
+                        writer.WriteLine("{0,10:6}, {1,10:6}:    {2}", positionedPoints[i, 0], positionedPoints[i, 1], songrefByMds[i]);
+                    }
+                }
+            }
+
+        }
+
+        private static Dictionary<int, SongRef> WellKnownTracksByMdsId(LastFmTools tools, CachedDistanceMatrix cachedMatrix) {
+            NiceTimer timer = new NiceTimer(); 
+            timer.TimeMark("Loading TrackMapper");
+            TrackMapper trainingMapper = SimilarTracks.LoadOnlyTrackMapper(tools, SimilarTracks.DataSetType.Training);
+            timer.TimeMark("Loading Billboard tracks");
+            var songrefBySqliteId = WellKnownTracksBySqliteId(tools);
+
+            var q =  //combines cachedMatrix.Mapping and trainingMapper and songrefBySqliteId to tuples (mdsId, songref)
+                from denseID in cachedMatrix.Mapping.CurrentlyMapped
+                let sqliteID = trainingMapper.LookupSqliteID(denseID)
+                where songrefBySqliteId.ContainsKey(sqliteID)
+                select new {
+                    MdsId = cachedMatrix.Mapping.GetMap(denseID),
+                    Song = songrefBySqliteId[sqliteID]
+                };
+
+            var retval = q.ToDictionary(kvp => kvp.MdsId, kvp => kvp.Song);
+            timer.Done();
+            return retval;
+        }
+
+        static Regex wellknown = new Regex(@"(billboard|top100)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+        private static IEnumerable<KeyValuePair<int, SongRef>> FindWellKnown(LastFmTools tools) {
+            
+            return
+                from songref in (
+                    from songdata in tools.DB.Songs
+                    where wellknown.IsMatch(songdata.SongPath)
+                    select SongRef.Create(songdata)).Distinct()
+                where songref != null
+                let trackID = tools.SimilarSongs.backingDB.LookupTrackID.Execute(songref)
+                where trackID.HasValue
+                select new KeyValuePair<int, SongRef>(trackID.Value, songref);
+        }
+
+        private static Dictionary<int, SongRef> WellKnownTracksBySqliteId(LastFmTools tools) {
+            return FindWellKnown(tools).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+
+
+        private static CachedDistanceMatrix LoadCachedMatrix(LastFmTools tools) {
+            NiceTimer timer = new NiceTimer();
             timer.TimeMark("Loading cached distance matrix");
             CachedDistanceMatrix cachedMatrix = CachedDistanceMatrix.LoadOrCache(tools.ConfigFile.DataDirectory);
-            timer.TimeMark("Saving"); 
+            timer.TimeMark("Saving matrix");
             cachedMatrix.Save();
-            timer.TimeMark("Loading strongly cached files");
+            timer.TimeMark("Loading strongly cached files into matrix");
             cachedMatrix.LoadDistFromAllCacheFiles();
-            timer.TimeMark("Saving"); 
+            timer.TimeMark("Saving matrix");
             cachedMatrix.Save();
             timer.Done();
+            return cachedMatrix;
+        }
 
+        private static double[,] DoMds(CachedDistanceMatrix cachedMatrix) {
+            Random r = new Random();
+            int dimensions=5;
+
+            float maxDist = cachedMatrix.Matrix.Values.Where(dist=>dist.IsFinite()) .Max();
+
+
+
+            using (Hitmds mdsImpl = new Hitmds(cachedMatrix.Mapping.Count, dimensions, (i, j) => cachedMatrix.Matrix[i, j].IsFinite()?cachedMatrix.Matrix[i, j]:maxDist*10, r)) {
+                mdsImpl.mds_train(cachedMatrix.Mapping.Count * 20, 2.0, 0.0, (cycle, ofTotal, src) => { });
+
+                double[,] retval = new double[cachedMatrix.Mapping.Count, dimensions];
+                for (int mdsId = 0; mdsId < cachedMatrix.Mapping.Count; mdsId++)
+                    for (int dim = 0; dim < dimensions; dim++)
+                        retval[mdsId, dim] = mdsImpl.GetPoint(mdsId, dim);
+                return retval;
+            }
         }
     }
 }
