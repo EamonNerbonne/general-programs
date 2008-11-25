4120b057bedd8342766f031078c94a59a7f694e1
eamon <devnull@localhost>
2008-11-25 13:56:24 +0000

    Converted web site to web app, now let's see if VS.NET is actually handier like this...

---
+++ SongSearchLinq/SongPlotterWebApp/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SongPlotterWebApp._Default" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Song Plotter</title>
+    <link rel="Stylesheet" href="songplot.css" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="m3uupload">Upload a playlist in .m3u format:
+        <asp:FileUpload ID="FileUpload1" runat="server"  /></div>
+    <div id="textbox" onmouseover="document.getElementById('TextBox1').focus()">Or enter a&nbsp; number of tracks as `Artist - Title':<br />
+        <asp:TextBox ID="TextBox1" TextMode="MultiLine"  AutoPostBack="false"  runat="server" Width="100%" Height="35em"/></div>
+    <div id="submitbutton"><asp:Button ID="PlotButton" runat="server" Text="Plot those songs" /></div>
+    <div id="unknown" <%=unknownSongs==null? "style='display:none;'":""%>><div style="float:right"><a href="#" onclick="document.getElementById('unknown').style.display='none';return false">close</a></div><center>I've never heard of: </center><br />
+        <asp:TextBox TextMode="MultiLine"  AutoPostBack="false" ID="UnknownBox2" runat="server" CssClass="textboxFix"></asp:TextBox></div>
+    <div id="map"><div style="position:absolute; top:5em;bottom:5em;left:5em;right:5em;" ><%PrintSongs(); %></div></div>
+    </form>
+</body>
+</html>
---
+++ SongSearchLinq/SongPlotterWebApp/Default.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using LastFMspider;
+using SimilarityMdsLib;
+using System.Windows;
+
+namespace SongPlotterWebApp
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        public static object syncroot = new object();
+        public static MdsResults mds;
+        [ThreadStatic]
+        public static LastFmTools tools; //this is thread static due to Sqlite restrictions.
+        void LoadStuff() {
+            lock (syncroot)
+                if (tools == null)
+                    tools = new LastFmTools(new SongDataLib.SongDatabaseConfigFile(false));
+            if (mds == null) {
+                mds = MdsResults.LoadResults(new MdsEngine.FormatAndOptions {
+                    Format = SimilarityFormat.AvgRank2,
+                    Options = new MdsEngine.Options {
+                        NGenerations = 140,
+                        LearnRate = 2.0,
+                        StartAnnealingWhen = 0.0,
+                        PointUpdateStyle = 1,
+                        Dimensions = 2,
+                    }
+                }, new SimCacheManager(SimilarityFormat.LastFmRating, tools, DataSetType.Training));
+            }
+        }
+        PositionedSong[] songs = null;
+        protected string[] unknownSongs = null;
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!this.IsPostBack) return;
+            LoadStuff();
+
+            //so this is a postback.
+            var playlist = LoadM3U.LoadPlaylistFromTextBlock(this.TextBox1.Text, tools);
+            if (FileUpload1.HasFile)
+                playlist = playlist.Concat(LoadM3U.LoadPlaylistFromM3U(this.FileUpload1.FileContent, tools));
+
+            //using (var m3ustream = File.OpenRead(openDialog.FileName)) {
+            PositionedTracks.PositionSongs(playlist, mds, out songs, out unknownSongs);
+            if (unknownSongs.Length == 0) unknownSongs = null;
+            TextBox1.Text = string.Join("\n", songs.Select(song => song.Song.ToString()).ToArray());
+            if (unknownSongs != null)
+                UnknownBox2.Text = string.Join("\n", unknownSongs);
+
+            PositionedTracks.RepositionWithin(songs, new Rect(0, 0, 100.0, 100.0));
+
+
+            //songCanvas.SetSongs(songs);
+            //}
+        }
+        protected override void OnUnload(EventArgs e) {
+            base.OnUnload(e);
+            LoadStuff();
+            songs = null;
+            if (unknownSongs != null) {
+                var backingDB = tools.SimilarSongs.backingDB;
+                try {
+                    using (var trans = backingDB.Connection.BeginTransaction()) {
+                        foreach (var songref in unknownSongs.SelectMany(unknownSong => LoadM3U.PossibleSongRefs(unknownSong)))
+                            backingDB.InsertTrack.Execute(songref);
+                    }
+                } catch {
+                    //if this fails, it's no biggy.  potentially, we should log it though... don't know how, though...
+                }
+            }
+            unknownSongs = null;
+
+        }
+        protected void PrintSongs() {
+            if (songs != null)
+                foreach (var song in songs) {
+                    Response.Write(string.Format(@"<div class=""song"" style=""left: {2}%;top:{3}%;""><table><tr><th>{0}</th></tr><tr><td>{1}</td></tr></table></div>",
+                        Server.HtmlEncode(song.Song.Artist),
+                        Server.HtmlEncode(song.Song.Title),
+                        song.MappedPosition.X,
+                        song.MappedPosition.Y));
+                }
+        }
+    }
+}
