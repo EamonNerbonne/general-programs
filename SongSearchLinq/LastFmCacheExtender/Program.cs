7b8a26cd70665928faa56c50638f0482b4d6f87f
eamon <devnull@localhost>
2008-11-29 14:10:05 +0000

    Moving/refactoring stage II (WARNING: broken build.)

--- SongSearchLinq/LastFmCacheExtender/Program.cs
+++ SongSearchLinq/LastFmCacheExtender/Program.cs
@@ -14,10 +14,33 @@ namespace LastFmCacheExtender
             Console.WriteLine("Loading song similarity...");
             //var similarSongs = new SongSimilarityCache(config);
             var tools = new LastFmTools(config);
-            tools.PrecacheArtistSimilarity();
-            tools.PrecacheArtistTopTracks();
-            tools.PrecacheSongSimilarity();
-            tools.PrecacheLocalFiles(false);//might want to do this first, but meh.
+            object tmp1=0, tmp2=1;
+            var dbcomm=tools.SimilarSongs.backingDB.Connection.CreateCommand();
+            var dbcomm2 = tools.SimilarSongs.backingDB.Connection.CreateCommand();
+            Console.WriteLine("Deleting similar tracks...");
+            dbcomm.CommandText = @"
+DELETE FROM SimilarTrack where TrackA in
+(select T.TrackID from  Artist A, Track T where A.ArtistID = T.TrackID AND (A.lowercaseArtist like '%.' OR T.LowercaseTitle LIKE '%.') )
+";
+            dbcomm2.CommandText = @"
+UPDATE Track SET LookupTimestamp = NULL WHERE TrackID in
+(select T.TrackID from  Artist A, Track T where A.ArtistID = T.TrackID AND (A.lowercaseArtist like '%.' OR T.LowercaseTitle LIKE '%.') )
+";
+            Console.WriteLine("made commands, executing...");
+            using (var trans = tools.SimilarSongs.backingDB.Connection.BeginTransaction()) {
+                Console.WriteLine("Deleting similar tracks...");
+                dbcomm.ExecuteNonQuery();
+                Console.WriteLine("Setting timestamp to null...");
+                dbcomm2.ExecuteNonQuery();
+                Console.WriteLine("Committing");
+                trans.Commit();
+            }
+            Console.WriteLine("done.");
+
+//            tools.PrecacheArtistSimilarity();
+  //          tools.PrecacheArtistTopTracks();
+    //        tools.PrecacheSongSimilarity();
+      //      tools.PrecacheLocalFiles(false);//might want to do this first, but meh.
         }
     }
 }
