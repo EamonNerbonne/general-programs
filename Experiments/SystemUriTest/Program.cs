99506ab5b71c3806a22247a522d9bfe5be74a6e9
eamon <devnull@localhost>
2008-12-13 12:52:19 +0000

    System.Uri testing.  I should probably make a work-around.

---
+++ Experiments/SystemUriTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemUriTest
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Uri a = new Uri("http://microsoft.com/dir/test.../file.txt");
+            Uri d = new Uri("http://microsoft.com/dir/test%2E%2E%2E/file.txt");
+            Uri e = new Uri("http://microsoft.com/dir/test/file.txt");
+
+
+            Console.WriteLine("{0}", string.Join(", ", new[] { a,  d }.Select(uri => (uri == e).ToString()).ToArray()));
+                
+
+        }
+    }
+}
