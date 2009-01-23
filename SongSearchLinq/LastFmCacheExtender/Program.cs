d90f91eb9ac22bb93a1b029e7474e6c819a49a10
eamon <devnull@localhost>
2009-01-23 19:27:41 +0000

    Removed parallelism in cache extender; will include in lastfmtools

--- SongSearchLinq/LastFmCacheExtender/Program.cs
+++ SongSearchLinq/LastFmCacheExtender/Program.cs
@@ -15,30 +15,18 @@ namespace LastFmCacheExtender
             
             var config = new SongDatabaseConfigFile(false);
             Console.WriteLine("Loading song similarity...");
-            //var similarSongs = new SongSimilarityCache(config);
             tools = new LastFmTools(config);
             tools.EnsureLocalFilesInDB();
             tools.UnloadDB();
-            object a, b, c, d, e, f, g,h;
             Console.WriteLine("go!");
-#if DEBUG
             ExtendSimilarities();
-#else
-
-            Parallel.Invoke(Enumerable.Repeat((Action)ExtendSimilarities, 4).ToArray());
-#endif
         }
 
         static void ExtendSimilarities() {
-        //    tools.PrecacheArtistTopTracks();
-            tools.PrecacheSongSimilarity();
-            //tools.PrecacheSongSimilarity();
             //tools.PrecacheLocalFiles(false);//might want to do this first, but meh.
-            //do {
-            //    tools.PrecacheArtistSimilarity();
-            //    tools.PrecacheArtistTopTracks();
-            //} while (tools.SimilarSongs.backingDB.ArtistsWithoutSimilarityList.Execute(1).Length > 0);
-            //tools.PrecacheSongSimilarity();
+            tools.PrecacheArtistSimilarity();
+            tools.PrecacheArtistTopTracks();
+            tools.PrecacheSongSimilarity();
         }
     }
 }
