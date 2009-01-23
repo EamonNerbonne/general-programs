8c9a88b8ad922a7ab37bfd9c1c8b0c89cfbff406
eamon <devnull@localhost>
2009-01-23 19:25:07 +0000

    Fixed spelling error in directory; tested out params in LINQ

---
+++ Experiments/LinqExperiments/LinqOutParams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqOutParams
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            int tmp=-1;
+            foreach (int i in
+                from num in Enumerable.Range(0, 100)
+                let str = num.ToString() + (num == 57 ? "x" : "")
+                let isInt = int.TryParse(str, out tmp)
+                where isInt
+                select tmp)
+                Console.WriteLine(i);
+        }
+    }
+}
