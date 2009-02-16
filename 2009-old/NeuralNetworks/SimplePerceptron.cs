b9fbd82eada18cc4517dc91dfdd44a4d3ba1f0c9
eamon <devnull@localhost>
2009-02-16 19:50:38 +0000

    Added original MinOver plots and MinOver source code with latex comments.

--- 2009-old/NeuralNetworks/SimplePerceptron.cs
+++ 2009-old/NeuralNetworks/SimplePerceptron.cs
@@ -64,84 +64,70 @@ namespace NeuralNetworks
 
 		public struct MinOverRes { public double Stability, BestStability;}
 		public MinOverRes DoMinOver(DataSet D, int maxEpochs) {
-			var dataPointOverlap = new double[D.P, D.P];
 
+			var dataPointOverlap = new double[D.P, D.P];
 			for (int j = 0; j < D.P; j++)
-				for (int i = 0; i < D.P; i++)
-					dataPointOverlap[i, j] = (D.samples[i].Sample & D.samples[j].Sample) 
+				for (int i = 0; i < D.P; i++) //initialize lookup table $L$
+					dataPointOverlap[i, j] = (D.samples[i].Sample & D.samples[j].Sample)
 						* (D.samples[i].Label * D.samples[j].Label) / N;
 
 
-			int minI = -1;
-			double min = double.MaxValue;
+			int  minI    = -1;               //the index of the currently minimally stable example
+			double minE  = double.MaxValue;  // the currently minimal local potential.
 
-			double[] localPotential = new double[D.P];//unscaled!
-			for (int j = 0; j < D.P; j++) { //initialize localPotential
-				localPotential[j] = (D.samples[j].Sample & w) * D.samples[j].Label;
-				if (localPotential[j] < min) {
+			double[] localPotential = new double[D.P]; //unscaled!
+			for (int j = 0; j < D.P; j++) { //initialize localPotential...
+				localPotential[j] = (D.samples[j].Sample & w) * D.samples[j].Label; //$E^\nu = w \cdot \xi^\nu S^\nu$
+				if (localPotential[j] < minE) {
 					minI = j;
-					min = localPotential[minI];
+					minE = localPotential[minI];
 				}
 			}
 			int resync = maxEpochs * D.P / 3;//resync to avoid numerical inaccuracies 
 
-			double wSqr = w & w;
-			double smoothFactor = 1 / 10000.0;
-			double smoothedStab = min/Math.Sqrt(wSqr);
-			double bestStab = min / Math.Sqrt(wSqr);
-			//double bestSqr = wSqr;
-		//	int lastUpdate =0;
-		//	int lastUpdateNG = 0;
-		//	int notGood=0;
+			double  wSqr         = w & w;                   //square of weight vector.
+			double bestStability = minE / Math.Sqrt(wSqr);  //maximal stability found so far
 
 			for (int n = 0; n < maxEpochs * D.P; n++) {
-				var wA = w.elems;
-				var sample = D.samples[minI].Sample.elems;
-				var scaleFac = D.samples[minI].Label / (double)N;
-				for (int i = 0; i < wA.Length; i++)
-					wA[i] += scaleFac * sample[i];
+				var wElems = w.elems;                                //$w(t)$
+				var minExampleElems = D.samples[minI].Sample.elems;  //$\xi^{\mu(t)}$
+				var scaleFac = D.samples[minI].Label / (double)N;    //$\frac{1}{N} S^{\mu(t)}$
+				for (int i = 0; i < wElems.Length; i++)              //$w(t+1) = w(t) + \frac{1}{N} S^{\mu(t)} \xi^{\mu(t)} $
+					wElems[i] += scaleFac * minExampleElems[i];
 
 				int curMinI = minI;
 				minI = -1;
-				min = double.MaxValue;
-				if ((n + 1) % resync == 0) {
+				minE = double.MaxValue;
+				if ((n + 1) % resync == 0) { //recompute wSqr and the local potentials to avoid numerical inaccuracy
 					for (int j = 0; j < localPotential.Length; j++) {
 						localPotential[j] = (D.samples[j].Sample & w) * D.samples[j].Label;
-						if (localPotential[j] < min) {
+						if (localPotential[j] < minE) {
 							minI = j;
-							min = localPotential[minI];
+							minE = localPotential[minI];
 						}
 					}
 					wSqr = w & w;
-				} else {
+				} else { //compute the new wSqr and new localPotentials incrementally, and find new minimum.
+					//$w(t+1)^2 = w(t)^2 + \frac{2}{N}E^{\mu(t)} + \frac{1}{N}L(\mu(t),\mu(t))$
 					wSqr += (2.0 * localPotential[curMinI] + dataPointOverlap[curMinI, curMinI]) / N;
 					for (int j = 0; j < localPotential.Length; j++) {
-						localPotential[j] += dataPointOverlap[curMinI, j];
-						if (localPotential[j] < min) {
+						localPotential[j] += dataPointOverlap[curMinI, j];//$E^\nu(t+1) = E^\nu(t) +  L\left(\mu(t), \nu\right)$
+						if (localPotential[j] < minE) {
 							minI = j;
-							min = localPotential[minI];
+							minE = localPotential[minI];
 						}
 					}
 				}
-				double oldSt = smoothedStab;
-				double currentStab = min/Math.Sqrt(wSqr);
-				//smoothedStab += smoothFactor * (min/Math.Sqrt(wSqr) - smoothedStab);
-				//if (smoothedStab < oldSt) //not good, going the wrong way!
-				//	notGood++;
 
-				if (currentStab > bestStab) {
-//					lastUpdate = n;
-//					lastUpdateNG = notGood;
-					bestStab = currentStab;
-					//bestSqr = wSqr;
-				}
-
+				double currentStability = minE / Math.Sqrt(wSqr);
+				if (currentStability > bestStability)
+					bestStability = currentStability;
 			}
+
 			return new MinOverRes {
-				BestStability = bestStab,
-				Stability = min / Math.Sqrt(w & w)
+				BestStability = bestStability,
+				Stability = minE / Math.Sqrt(w & w)
 			};
 		}
-
 	}
 }
