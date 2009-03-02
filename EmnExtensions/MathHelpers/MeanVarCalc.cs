0be7a0d4269404739226a7beeb4b39a25c64739a
eamon <devnull@localhost>
2009-03-02 18:21:55 +0000

    MeanVarCalc now can add another MeanVarCalc to itself.

--- EmnExtensions/MathHelpers/MeanVarCalc.cs
+++ EmnExtensions/MathHelpers/MeanVarCalc.cs
@@ -16,6 +16,7 @@ namespace EmnExtensions.MathHelpers
 			sum += val;
 			sum2 += val * val;
 		}
+		public void Add(MeanVarCalc other) {			Add(other.count, other.sum, other.sum2);	}
 		public void Add(int count, double sum, double sumSquared) {
 			this.count += count;
 			this.sum += sum;
@@ -26,6 +27,7 @@ namespace EmnExtensions.MathHelpers
 		//need Math.Max since it's possible rounding errors cause negative numbers!
 		public double Var { get { return Math.Max(0,(sum2 - Mean * Mean * count) / (count - 1)); } }
 		public double StdDev { get { return Math.Sqrt(Var); } }
+		public int Count { get { return count; } }
 
 		public override string ToString() {			return Mean.ToString() + " +/- " + StdDev.ToString();		}
 	}
