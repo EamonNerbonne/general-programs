f4273054f7d326d1326dc09a08e56a2d9df1d1b9
eamon <devnull@localhost>
2009-04-22 07:35:06 +0000

    LVQeamon project added.

---
+++ LvqEmn/CreateGaussianCloud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EmnExtensions.MathHelpers;
+
+namespace LVQeamon
+{
+	public static class CreateGaussianCloud
+	{
+
+		public static double[,] GaussianCloud(int numPoints, int numDimensions, double[,] covarianceMatrix, double[] mean, MersenneTwister rand) {
+			
+		}
+
+	}
+}
