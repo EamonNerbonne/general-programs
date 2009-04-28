3149c061ed26657c343747b0d6cd60b4ef5a4bb5
eamon <devnull@localhost>
2009-04-28 10:57:31 +0000

    Removed reference to outdated PersistantCache Added sqlite dependency to LastFMspider/LastFmCacheExtender/App.config - this way no GAC-ing needed.

--- SongSearchLinq/LastFMspider/SongSimilarityCache.cs
+++ SongSearchLinq/LastFMspider/SongSimilarityCache.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using EmnExtensions.PersistantCache;
 using System.IO;
 using System.Xml.Linq;
 using LastFMspider.LastFMSQLiteBackend;
