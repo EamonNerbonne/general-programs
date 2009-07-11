3fcbcaf8932c4c734de0540eed74de2e1151b595
eamon <devnull@localhost>
2009-07-11 11:24:07 +0000

    Added initial experimental EDM model of LastFMcache - seems to work, anyhow.

---
+++ SongSearchLinq/LastFMentityModel/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LastFMentityModel
+{
+	class TestClass
+	{
+		static void Test() {
+			using (LastFMCacheModel model = new LastFMCacheModel()) {
+				var q = from artist in model.Artist
+						let ttl = artist.CurrentTopTracksList
+						where ttl !=null 
+						let overReach = ttl.TopTracks.Sum(tt=>tt.Reach)
+						orderby overReach descending
+						select artist;
+
+				foreach (var artist in q.Take(100)) {
+					Console.WriteLine(artist);
+				}
+			}
+		}
+
+		static void Main(string[] args) {
+			Test();
+		}
+	}
+}
