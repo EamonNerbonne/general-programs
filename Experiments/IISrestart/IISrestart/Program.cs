1f06f84acb503d7de40f2bb029e12f3239fc5688
eamon <devnull@localhost>
2009-08-13 16:17:33 +0000

    network stuffs

---
+++ Experiments/IISrestart/IISrestart/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+
+namespace IISrestart
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		static void RestartIIS()
+		{
+			ServiceController w3svc = ServiceController.GetServices().First(service => service.ServiceName.ToUpperInvariant() == "W3SVC");
+			Console.Write("Stopping");
+			w3svc.Stop();
+			Console.WriteLine("...");
+			w3svc.WaitForStatus(ServiceControllerStatus.Stopped);
+			Console.Write("Restarting");
+			w3svc.Start();
+			Console.WriteLine("...");
+			w3svc.WaitForStatus(ServiceControllerStatus.Running);
+			Console.WriteLine("Restarted.");
+		}
+	}
+}
