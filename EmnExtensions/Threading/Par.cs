deaa61cd0db271eaf5c78352570f3b7a7e37903c
eamon <devnull@localhost>
2009-09-25 14:06:57 +0000

    Added simplistic custom parallelizable Invoke to tide me over until .NET 4.0 is ready for primetime.

---
+++ EmnExtensions/Threading/Par.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EmnExtensions.Threading
+{
+	public static class Par
+	{
+		public static void Invoke(params Action[] actions) {
+			using (Semaphore sem = new Semaphore(0, actions.Length)) {
+				foreach (Action action in actions) {
+					var localaction = action;
+					ThreadPool.QueueUserWorkItem(ignore => {
+						try { localaction(); } finally { sem.Release(); }
+					});
+				}
+				foreach (Action a in actions)
+					sem.WaitOne();
+			}
+		}
+	}
+}
