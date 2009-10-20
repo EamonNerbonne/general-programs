687330dbe89faaa46d02477450c33f6294d79bc8
eamon <devnull@localhost>
2009-10-20 12:10:27 +0000

    updated eigentest to use unstable version of eigen.

--- Experiments/PlayingWithEigen/EigenTest/EigenTest.cpp
+++ Experiments/PlayingWithEigen/EigenTest/EigenTest.cpp
@@ -25,21 +25,16 @@ int _tmain(int argc, _TCHAR* argv[])
 		using boost::scoped_ptr;
 		
 		Matrix2d A(Matrix2d::Identity());//value irrelevant
-        scoped_ptr<BuggyAlignment> B(new BuggyAlignment(A));
 
 		Vector2d v;
 		v<< 3,5;//irrelevant
 		
         cout <<"align(A):"<< long long(&A) %16<<endl; //0
-		cout <<"align(B->mat):"<< long long(&(B->mat)) %16<<endl;//0
-		cout <<"align(B->mat):"<< long long(&(B->ignore5)) %16<<endl;//1
-		cout <<"align(B->ignore5):"<< (void*)B.get()<<", "<< (void*)&B.get()->ignore<<", "<< (void*)&B.get()->mat <<", "<< (void*)&B.get()->ignore2<<", "<< (void*)&B.get()->ignore5  <<endl;//0??
 
 		cout<<"Trying:  A^T * v\n";
 		Vector2d w1 = A.transpose() * v; //fails in x64 with vectorization, succeeds in x64 without vectorization, succeeds in x86 with or without vectorization
 
 		cout<< w1 << endl;
-		w1 += B->mat * v; //alignement error works fine under debug mode.
 		cout<<"END-TEST\n\n\n";
 
 	return 0;
