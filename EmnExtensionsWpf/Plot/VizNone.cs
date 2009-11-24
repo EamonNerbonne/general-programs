3c6f12c6a9a0a97dfb7adc1f9a01baf56c5dad00
eamon <devnull@localhost>
2009-11-24 16:00:23 +0000

    old plot control copied to new plot control, so far, still using old-style plot data.

---
+++ EmnExtensionsWpf/Plot/VizNone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace EmnExtensions.Wpf.Plot
+{
+	public class VizNone : IPlotViz
+	{
+		private VizNone() { }
+		private static readonly VizNone singleton = new VizNone();
+		public VizNone Singleton { get { return singleton; } }
+		public Rect DataBounds { get { return Rect.Empty; } }
+		public Thickness Margin { get { return new Thickness(0.0); } }
+		public void DrawGraph(DrawingContext context) { }
+		public void SetTransform(Matrix boundsToDisplay, Rect displayClip) { }
+		public void DataChanged(object newData) { }
+		public void SetOwner(IPlotVizOwner owner) { }
+	}
+}
