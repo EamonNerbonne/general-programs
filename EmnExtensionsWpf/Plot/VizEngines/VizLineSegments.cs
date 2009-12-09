453ec97e978b6decf3607479b8efc6d42fc09017
eamon <devnull@localhost>
2009-12-09 14:12:04 +0000

    Implemented a line segment viz for point data.

---
+++ EmnExtensionsWpf/Plot/VizEngines/VizLineSegments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace EmnExtensions.Wpf.Plot.VizEngines
+{
+	class VizLineSegments : PlotVizTransform<Point[], StreamGeometry>
+	{
+		IVizEngine<StreamGeometry> impl = new VizGeometry();
+		StreamGeometry geomCache;
+		protected override StreamGeometry TransformedData(Point[] inputData) { return geomCache; }
+
+		protected override IVizEngine<StreamGeometry> Implementation { get { return impl; } }
+
+		public override void DataChanged(Point[] newData)
+		{
+			geomCache = GraphUtils.LineUnscaled (newData);
+			impl.DataChanged(geomCache);
+		}
+	}
+}
