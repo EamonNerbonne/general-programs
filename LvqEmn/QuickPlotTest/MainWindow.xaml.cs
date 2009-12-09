9ebfcd887c378c0288b851db76a9dc68976e3dab
eamon <devnull@localhost>
2009-12-09 14:15:52 +0000

    QuickPlotTest shows it works for both scatter and line plots.

--- LvqEmn/QuickPlotTest/MainWindow.xaml.cs
+++ LvqEmn/QuickPlotTest/MainWindow.xaml.cs
@@ -11,22 +11,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EmnExtensions.Wpf.Plot;
+using EmnExtensions.MathHelpers;
 
 namespace QuickPlotTest
 {
+	public static class QuickHelpers
+	{
+		public static T With<T>(this T item, Action<T> action) { action(item); return item; }
+	}
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+		public MainWindow() { InitializeComponent(); }
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
+			Random r = new MersenneTwister(37);
 			newPlotControl1.Graphs.Add(
+				PlotData.Create(
+						(from index in Enumerable.Range(0, 1000)
+						 let x = index
+						 select new Point(x, x *(double) x)).ToArray()
+					).With(plot =>
+					{
+						plot.PlotClass = PlotClass.Line;
+						plot.DataLabel = "parabola";
+						plot.XUnitLabel = "x";
+						plot.YUnitLabel = "x^2";
+					}
+					)
+				);
+
+			newPlotControl1.Graphs.Add(
+				PlotData.Create(
+						(from index in Enumerable.Range(0, 1000)
+						 from tries in Enumerable.Range(0, 500)
+						 let x = index + r.NextNorm()
+						 let y = (x + r.NextNorm()*5) * (x + r.NextNorm()*5)
+						 select new Point(x, y)).ToArray()
+					).With(plot =>
+					{
+						plot.DataLabel = "parabola";
+						plot.XUnitLabel = "x";
+						plot.YUnitLabel = "x^2";
+					})
+				);
 		}
 	}
 }
