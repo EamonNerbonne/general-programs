9c18847501dcff4c5d8749a32b34c51a4db2e5f1
eamon <devnull@localhost>
2010-03-05 11:00:28 +0000

    another micro-perf test

--- LvqEmn/EigenBench/EigenBench.cpp
+++ LvqEmn/EigenBench/EigenBench.cpp
@@ -6,6 +6,7 @@
 #include "subtractBench.h"
 #include "matmulTest.h"
 #include "copyVecTest.h"
+#include "prodNormTest.h"
 
 #include <fstream>
 //from http://www.codeproject.com/KB/files/filesize.aspx
@@ -49,8 +50,18 @@ int main(int , char*argv []){
 #endif
 	cout<<": ";
 
-	//projectionTest();
-	//subtractTest();
-	cout << copyVecTest() <<"s; "<< file_size(argv[0])/1024 <<"KB\n";
+	cout << prodNormTest() <<"s; "<< file_size(argv[0])/1024 <<"KB\n";
+	//Matrix<double,Dynamic,Dynamic,0,16,16> test= MatrixXd::Random(3,17);
+	//Matrix4d b,c,d;
+	//b.setZero();
+	//c.setIdentity();
+	//d.setConstant(0.5);
+	//test = MatrixXd::Random(3,17);
+	//cerr << "test:"<<test<<"\n\n";
+	//cerr << "b:"<<b<<"\n\n";
+	//cerr << "c:"<<c<<"\n\n";
+	//cerr << "d:"<<d<<"\n\n";
+	//cerr<< "sizeof(test)" <<sizeof(test) <<"\n";
+
 	return 0; 
 }
---
+++ LvqEmn/EigenBench/prodNormTest.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "standard.h"
+
+double prodNormTest() {
+	BenchTimer t;
+	double sum=0.0;
+	for(int bI=0;bI<BENCH_RUNS;bI++) {
+
+		VectorXd a = VectorXd::Random(DIMS);
+		Matrix<double,Eigen::Dynamic,Eigen::Dynamic,0,Eigen::Dynamic,Eigen::Dynamic> mat = MatrixXd::Random(DIMS,DIMS);
+
+		const int num_runs = 20000000/DIMS ;// 20000000;
+		t.start();
+		for (int i=0; i<num_runs; ++i) {
+#if EIGEN3
+			sum += (mat*a).squaredNorm();
+#else
+			sum += (mat*a).squaredNorm();
+#endif
+			a(i%DIMS) *= 0.5;
+		}
+		t.stop();
+	}
+	cout <<"(" << sum <<") ";
+	return t.best();
+}
