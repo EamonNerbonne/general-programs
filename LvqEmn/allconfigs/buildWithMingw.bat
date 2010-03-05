5328bd715afc29f48dfe915b2543b677b20ee515
eamon <devnull@localhost>
2010-03-05 12:53:29 +0000

    low-priority build doesn't really work.

--- LvqEmn/allconfigs/buildWithMingw.bat
+++ LvqEmn/allconfigs/buildWithMingw.bat
@@ -2,5 +2,6 @@
 mkdir %~dp0\..\build\mingw
 cd %~dp0\..\build\mingw
 
-start /belownormal /wait /B cmake -G "MinGW Makefiles" ..\..\allconfigs
-start /belownormal /wait /B mingw32-make -j
+REM start /belownormal /wait /B
+cmake -G "MinGW Makefiles" ..\..\allconfigs
+mingw32-make -j
--- LvqEmn/allconfigs/buildWithMsc.bat
+++ LvqEmn/allconfigs/buildWithMsc.bat
@@ -2,5 +2,5 @@
 mkdir %~dp0\..\build\msc
 cd %~dp0\..\build\msc
 
-start /belownormal /wait /B cmake -G "Visual Studio 10 Win64" ..\..\allconfigs
-start /belownormal /wait /B C:\Windows\Microsoft.NET\Framework64\v4.0.30128\MSBuild.exe "/property:Configuration=Release" "/maxcpucount" "ALL_BUILD.vcxproj"
+cmake -G "Visual Studio 10 Win64" ..\..\allconfigs
+C:\Windows\Microsoft.NET\Framework64\v4.0.30128\MSBuild.exe /property:Configuration=Release /maxcpucount "ALL_BUILD.vcxproj"
