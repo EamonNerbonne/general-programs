6b95e435f0e4670d657dab8fdb5ce38fec863022
eamon <devnull@localhost>
2010-04-08 21:35:10 +0000

    CompilerError simplified; this version posted to Microsoft Connect

--- Experiments/PlayingWithEigen/MscCompilerError/MscCompilerError.cpp
+++ Experiments/PlayingWithEigen/MscCompilerError/MscCompilerError.cpp
@@ -3,39 +3,37 @@ template <typename T> struct Params;
 
 template<typename TDerived>
 struct GeneralBase  {
-	enum{ IsVector = Params<TDerived>::IsVector  };
+	enum{ ParamVal = Params<TDerived>::ParamVal  };
+	typedef TDerived Derived;
 };
-
 
 template <int val>
 struct Container : GeneralBase<Container<val > > {
 	typedef GeneralBase<Container<val > > Base;
-	using Base::IsVector; //causes ICE!
-	//doesn't cause ICE:
-	//enum{ IsVector = Params<Base>::IsVector  };
-	int getSize() {return val;}
-};
+	int getVal() {return val;}
 
-template <typename TDerived>
-struct Params<GeneralBase<TDerived> > {
-	enum{ IsVector = Params<TDerived>::IsVector };
+	using Base::ParamVal; //triggers bug
+	//enum{ ParamVal = Params<typename Base::Derived>::ParamVal  };	//doesn't trigger bug
 };
 
 template <int val>
 struct Params<Container< val> > {
-	enum{ IsVector = (val == 1) };
+	enum{ ParamVal = val };
 };
 
+#define CAUSE_CRASH (Container<val>::ParamVal?0:1)
+#define CAUSE_C2135 (!Container<val>::ParamVal)
+#define CAUSE_C2597 (Container<val>::ParamVal + 1)
+
+#define TEMPLATE_PARAM CAUSE_CRASH
+
 template<int val>
-Container</*required for ICE:*/ (Container<val>::IsVector?1:2)> ConvertX(Container<val> const & x){ 
-	Container<(Container<val>::IsVector?1:2)> nx;
-	return nx;
+Container<TEMPLATE_PARAM> ConvertX(Container<val> const & x){ 
+	return Container<TEMPLATE_PARAM>();
 }
 
-int main()
-{
-	Container<2> v2i;
-	printf("%d\n", ConvertX(v2i).getSize() ); //8
-
+int main() {
+	Container<0> v2i;
+	printf("%d\n", ConvertX(v2i).getVal() ); //should print 1
     return 0;
 }
