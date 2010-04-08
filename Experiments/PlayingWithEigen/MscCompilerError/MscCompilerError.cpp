083b3baee1c6380246062987515a61217ac88350
eamon <devnull@localhost>
2010-04-08 20:18:48 +0000

    Simplified ICE

--- Experiments/PlayingWithEigen/MscCompilerError/MscCompilerError.cpp
+++ Experiments/PlayingWithEigen/MscCompilerError/MscCompilerError.cpp
@@ -4,7 +4,6 @@ template <typename T> struct Params;
 template<typename TDerived>
 struct GeneralBase  {
 	enum{ IsVector = Params<TDerived>::IsVector  };
-	typedef typename Params<TDerived>::ValueType ValueType;
 };
 
 
@@ -15,29 +14,27 @@ struct ContainerBase : GeneralBase<TDerived> {
 	//doesn't cause ICE:
 	//enum{ IsVector = Params<TDerived>::IsVector  };
 	
-	typedef typename Params<TDerived>::ValueType ValueType;
 };
 
-template <typename T, int dims>
-struct Container : ContainerBase<Container<T, dims> > {
-	int getSize() {return dims*sizeof(T);}
+template < int dims>
+struct Container : ContainerBase<Container< dims> > {
+	int getSize() {return dims;}
 };
 
-template <typename T, int dims>
-struct Params<Container<T, dims> > {
-	typedef T ValueType;
+template < int dims>
+struct Params<Container< dims> > {
 	enum{ IsVector = (dims == 1) };
 };
 
 template<typename TDerived>
-Container<typename ContainerBase<TDerived>::ValueType, /*required for ICE:*/ (ContainerBase<TDerived>::IsVector?1:2)> ConvertX(ContainerBase<TDerived> const & x){ 
-	Container<typename ContainerBase<TDerived>::ValueType, (ContainerBase<TDerived>::IsVector?1:2)> nx;
+Container</*required for ICE:*/ (ContainerBase<TDerived>::IsVector?1:2)> ConvertX(ContainerBase<TDerived> const & x){ 
+	Container<(ContainerBase<TDerived>::IsVector?1:2)> nx;
 	return nx;
 }
 
 int main()
 {
-	ContainerBase<Container<int,2>> v2i;
+	ContainerBase<Container<2>> v2i;
 	printf("%d\n", ConvertX(v2i).getSize() ); //8
 
     return 0;
