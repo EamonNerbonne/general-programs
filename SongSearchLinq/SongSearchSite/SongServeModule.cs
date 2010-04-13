6eb15bc861bbad104749b2bec5b489e6b88c5ecb
eamon <devnull@localhost>
2010-04-13 12:25:06 +0000

    Song serving updated to limit bandwidth to 400kbps.

--- SongSearchLinq/SongSearchSite/SongServeModule.cs
+++ SongSearchLinq/SongSearchSite/SongServeModule.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Web;
-using System.Web.SessionState;
+//using System.Web.SessionState;
 using HttpHeaderHelper;
 using SongDataLib;
+using System.Diagnostics;
+using System.Threading;
 
 namespace SongSearchSite
 {
@@ -19,26 +21,30 @@ namespace SongSearchSite
 
 		public PotentialResourceInfo DetermineResource() {
 			string reqPath = helper.Context.Request.AppRelativeCurrentExecutionFilePath;
-			if(!reqPath.StartsWith(prefix)) throw new Exception("Whoops, illegal request routing...  this should not be routed to this class!");
+			if (!reqPath.StartsWith(prefix))
+				throw new Exception("Whoops, illegal request routing...  this should not be routed to this class!");
 
 			string songNormedPath = reqPath.Substring(prefix.Length);
 			song = SongContainer.GetSongByNormalizedPath(songNormedPath);
-			if(song == null) return new ResourceError() {
-				Code = 404,
-				Message = "Could not find file '" + songNormedPath + "'.  Path is not indexed."
-			};
+			if (song == null)
+				return new ResourceError() {
+					Code = 404,
+					Message = "Could not find file '" + songNormedPath + "'.  Path is not indexed."
+				};
 
 			FileInfo fi = new FileInfo(song.SongPath);
 
-			if(!fi.Exists) return new ResourceError() {
-				Code = 404,
-				Message = "Could not find file '" + song.SongPath + "' even though it's indexed as '" + songNormedPath + "'. Sorry.\n"
-			};
+			if (!fi.Exists)
+				return new ResourceError() {
+					Code = 404,
+					Message = "Could not find file '" + song.SongPath + "' even though it's indexed as '" + songNormedPath + "'. Sorry.\n"
+				};
 
-			if(fi.Length > Int32.MaxValue) return new ResourceError() {
-				Code = 413,
-				Message = "Requested File " + song.SongPath + " is too large!"
-			};	//should now actually support Int64's, but to be extra cautious
+			if (fi.Length > Int32.MaxValue)
+				return new ResourceError() {
+					Code = 413,
+					Message = "Requested File " + song.SongPath + " is too large!"
+				};	//should now actually support Int64's, but to be extra cautious
 			//this should never happen, and never be necessary, but just to be sure...
 
 
@@ -61,26 +67,60 @@ namespace SongSearchSite
 			get { return true; }
 		}
 
-		public void WriteByteRange(Range range) {
+		public void WriteByteRange(Range range) { WriteHelper(range); }
+
+
+
+		public void WriteEntireContent() { WriteHelper(null); }
+
+		public void WriteHelper(Range? range) {
+			const int maxBytePerSec = 51200;//400kbps //alternative: extract from tag using taglib.  However, this isn't always the right (VBR) bitrate, and may thus fail.
+			const int window = 4096;
+			byte[] buffer = new byte[window];
+			Stopwatch timer = Stopwatch.StartNew();
 			helper.Context.Response.Buffer = false;
-			helper.Context.Response.TransmitFile(song.SongPath, range.start, range.length);
+			long end = range.HasValue ? range.Value.start + range.Value.length : long.MaxValue;
+			long start = range.HasValue ? range.Value.start : 0;
+			bool streamEnded = false;
+			using (var stream = new FileStream(song.SongPath, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+				stream.Seek(start, SeekOrigin.Begin);
+				while (!streamEnded && stream.Position < end && helper.Context.Response.IsClientConnected) {
+					long maxPos = start + (long)(timer.Elapsed.TotalSeconds * maxBytePerSec)+window*4;
+					long excessBytes = stream.Position - maxPos;
+					if (excessBytes > 0) {
+						Thread.Sleep(TimeSpan.FromSeconds(excessBytes / (double)maxBytePerSec));
+					}
+
+					int bytesToRead = (int)Math.Min((long)window, end - stream.Position);
+					int i = 0;
+					while (i < bytesToRead && helper.Context.Response.IsClientConnected) {
+						int bytesRead = stream.Read(buffer, i, bytesToRead - i);
+						if (bytesToRead == 0) {
+							streamEnded = true;
+							break;
+						}
+						helper.Context.Response.OutputStream.Write(buffer, i, bytesRead);
+						i += bytesToRead;
+					}
+				}
+			}
 		}
 
 
-
-		public void WriteEntireContent() {
-			helper.Context.Response.Buffer = false;
-			helper.Context.Response.TransmitFile(song.SongPath);
-		}
 		public static string guessMIME(string extension) {
-			switch(extension.ToLowerInvariant()) {
-				case ".mp3": return "audio/mpeg";
-				case ".wma": return "audio/x-ms-wma";
-				case ".wav": return "audio/wav";
-				case ".ogg": return "application/ogg";
+			switch (extension.ToLowerInvariant()) {
+				case ".mp3":
+					return "audio/mpeg";
+				case ".wma":
+					return "audio/x-ms-wma";
+				case ".wav":
+					return "audio/wav";
+				case ".ogg":
+					return "application/ogg";
 				case ".mpc":
 				case ".mpp":
-				case ".mp+": return "audio/x-musepack";
+				case ".mp+":
+					return "audio/x-musepack";
 				default:
 					return "application/octet-stream";//TODO fix!
 			}
@@ -91,7 +131,7 @@ namespace SongSearchSite
 	/// <summary>
 	/// Summary description for SongServeModule
 	/// </summary>
-	public class SongServeHandler : IHttpHandler, IRequiresSessionState
+	public class SongServeHandler : IHttpHandler
 	{
 		public bool IsReusable { get { return true; } }
 
