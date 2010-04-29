1ad0454e648663da658be6957440dddb70f38351
eamon <devnull@localhost>
2010-04-29 21:37:31 +0000

    MusicDirCleanup better cleans up gunk like accompanying images.

--- MusicDirCleanup/MusicDirCleanup/Program.fs
+++ MusicDirCleanup/MusicDirCleanup/Program.fs
@@ -1,11 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.net
-open System.IO
 open EmnExtensions.Filesystem
+open System.IO
 open Microsoft.FSharp.Collections
 
-let moveToParent kid =
-    let newPath = Path.GetDirectoryName kid + @"\..\" + Path.GetFileName kid |> Path.GetFullPath 
-    Directory.Move (kid, newPath)
  
 let clearReadOnly d = File.SetAttributes (d,  File.GetAttributes d &&& ~~~FileAttributes.ReadOnly &&& ~~~FileAttributes.System)
 
@@ -13,33 +10,41 @@ let isInSet setElems normalize = normalize >> (setElems |> List.map normalize |>
 let getLowerExtension path = (Path.GetExtension path).ToLowerInvariant()
 
 let isBad = isInSet [".m3u"; ".nfo"; ".ini"; ".txt"; ".sfv"; ".bak"; ".url"; ".old"; ".pls"; ] getLowerExtension
-let isIgnores =  isInSet [".jpg"; ".jpeg"; ] getLowerExtension
+let isIgnores =  isInSet [".jpg"; ".jpeg"; ".db" ] getLowerExtension
 
 let MusicDir = "E:\\Music"
 let Keepers = MusicDir :: List.ofSeq (Directory.EnumerateDirectories MusicDir)
 let isKeeper =  isInSet Keepers (fun s -> Path.GetFullPath (s + @"\"))
 
+let moveToParent kid =
+    let newPath = Path.GetDirectoryName kid + @"\..\" + Path.GetFileName kid |> Path.GetFullPath 
+    if File.Exists newPath then
+        File.Delete newPath
+    Directory.Move (kid, newPath)
 
 
+let delete x =
+    File.Delete x
 
 let rec processDir d =
-    let contents = Directory.EnumerateFileSystemEntries d
-    contents |> Seq.iter clearReadOnly
-    Seq.filter Directory.Exists contents |> Seq.iter processDir
+    let contents = Directory.EnumerateFileSystemEntries d //lazily evaluated!
+    contents |> Seq.iter clearReadOnly 
+    contents |> Seq.filter Directory.Exists |> Seq.iter processDir
     try 
-        contents |> Seq.filter File.Exists |> Seq.filter isBad |> Seq.iter File.Delete
+        contents |> Seq.filter File.Exists |> Seq.filter isBad |> Seq.iter delete
         if not <| isKeeper d then
-            match List.ofSeq contents with
-                | [] -> 
-                    Directory.Delete d
+            match contents |> Seq.filter (isIgnores >> not) |> List.ofSeq with
+                | [] -> Directory.Delete (d, true) //only contains ignored items, OK to delete.
                 | [ onlyKid ] ->
                     if Directory.Exists onlyKid then
                         onlyKid |> Directory.EnumerateFileSystemEntries |> Seq.iter moveToParent
                         Directory.Delete (onlyKid, false)
                     else
-                        moveToParent onlyKid
-                | _ -> 
-                    ignore 1
+                        contents |> Seq.iter moveToParent
+                        if Seq.length contents > 0 then
+                            failwith "whaaaa?"
+                        Directory.Delete (d, false)
+                | _ -> ()
     with
         | :? System.IO.IOException as e ->
             printfn "error in %s: %s" d e.Message
