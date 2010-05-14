f9acd56baaabcccfeeb58b0d882f9d2e8e35d335
eamon <devnull@localhost>
2010-05-14 15:19:06 +0000

    Bugfix: wrong param names

--- SongSearchLinq/LastFMspider/LastFMSQLiteBackend/InsertArtist.cs
+++ SongSearchLinq/LastFMspider/LastFMSQLiteBackend/InsertArtist.cs
@@ -9,7 +9,7 @@ namespace LastFMspider.LastFMSQLiteBackend {
 		protected override string CommandText {
 			get { return @"
 INSERT OR IGNORE INTO [Artist](FullArtist, LowercaseArtist) VALUES (@fullname,@lowername);
-SELECT ArtistID FROM [Artist] WHERE LowercaseArtist = @lowerArtist
+SELECT ArtistID FROM [Artist] WHERE LowercaseArtist = @lowername
 "; }
 		}
 		public InsertArtist(LastFMSQLiteCache lfm)
