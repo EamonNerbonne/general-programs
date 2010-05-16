7ce70a7be86fbdf5e2236f9c5ce6ae91697f5602
eamon <devnull@localhost>
2010-05-16 14:16:00 +0000

    (minor) project file +indenting fix.

--- SongSearchLinq/LastFMspider/LastFmTools.cs
+++ SongSearchLinq/LastFMspider/LastFmTools.cs
@@ -12,67 +12,65 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace LastFMspider
-{
-    public class LastFmTools
-    {
-        SongSimilarityCache similarSongs;
-        SongDatabaseConfigFile configFile;
-        SimpleSongDB db;
-        SongDataLookups lookup;
-        //  private object sync = new object();
+namespace LastFMspider {
+	public class LastFmTools {
+		SongSimilarityCache similarSongs;
+		SongDatabaseConfigFile configFile;
+		SimpleSongDB db;
+		SongDataLookups lookup;
+		//  private object sync = new object();
 
-        public SongSimilarityCache SimilarSongs {
-            get {
-                // lock (sync)
-                return similarSongs ?? (similarSongs = new SongSimilarityCache(ConfigFile));
-            }
-        }
-        public SongDatabaseConfigFile ConfigFile { get { return configFile; } }
-        public SimpleSongDB DB {
-            get {
-                // lock (sync)
-                return db ?? (db = new SimpleSongDB(ConfigFile, null));
-            }
-        }
+		public SongSimilarityCache SimilarSongs {
+			get {
+				// lock (sync)
+				return similarSongs ?? (similarSongs = new SongSimilarityCache(ConfigFile));
+			}
+		}
+		public SongDatabaseConfigFile ConfigFile { get { return configFile; } }
+		public SimpleSongDB DB {
+			get {
+				// lock (sync)
+				return db ?? (db = new SimpleSongDB(ConfigFile, null));
+			}
+		}
 
-        public SongDataLookups Lookup {
-            get {
-                //  lock (sync)
-                return lookup ?? (lookup = new SongDataLookups(DB.Songs, null));
-            }
-        }
+		public SongDataLookups Lookup {
+			get {
+				//  lock (sync)
+				return lookup ?? (lookup = new SongDataLookups(DB.Songs, null));
+			}
+		}
 
-        public LastFmTools(SongDatabaseConfigFile configFile=null) {
-            this.configFile = configFile ?? new SongDatabaseConfigFile(true);
-        }
+		public LastFmTools(SongDatabaseConfigFile configFile = null) {
+			this.configFile = configFile ?? new SongDatabaseConfigFile(true);
+		}
 
 
-        public void UnloadLookup() {
-            // lock (sync)
-            lookup = null;
-        }
+		public void UnloadLookup() {
+			// lock (sync)
+			lookup = null;
+		}
 
-        public void UnloadDB() {
-            //  lock (sync) {
-            UnloadLookup();
-            db = null;
-            // }
-        }
+		public void UnloadDB() {
+			//  lock (sync) {
+			UnloadLookup();
+			db = null;
+			// }
+		}
 
-        /// <summary>
-        /// Downloads Last.fm metadata for all tracks in the song database (if not already present).
-        /// </summary>
-        /// <param name="shuffle">Whether to perform the precaching in a random order.  Doing so slows down the precaching when almost all
-        /// items are already downloaded, but permits multiple download threads to run in parallel without duplicating downloads.</param>
-        public void PrecacheLocalFiles(bool shuffle = false) { ToolsInternal.PrecacheLocalFiles(this, shuffle); }
+		/// <summary>
+		/// Downloads Last.fm metadata for all tracks in the song database (if not already present).
+		/// </summary>
+		/// <param name="shuffle">Whether to perform the precaching in a random order.  Doing so slows down the precaching when almost all
+		/// items are already downloaded, but permits multiple download threads to run in parallel without duplicating downloads.</param>
+		public void PrecacheLocalFiles(bool shuffle = false) { ToolsInternal.PrecacheLocalFiles(this, shuffle); }
 
-        public void EnsureLocalFilesInDB() { ToolsInternal.EnsureLocalFilesInDB(this); }
+		public void EnsureLocalFilesInDB() { ToolsInternal.EnsureLocalFilesInDB(this); }
 
-        public int PrecacheSongSimilarity() { return ToolsInternal.PrecacheSongSimilarity(this); }
+		public int PrecacheSongSimilarity() { return ToolsInternal.PrecacheSongSimilarity(this); }
 
-        public int PrecacheArtistSimilarity() { return ToolsInternal.PrecacheArtistSimilarity(this); }
+		public int PrecacheArtistSimilarity() { return ToolsInternal.PrecacheArtistSimilarity(this); }
 
-        public int PrecacheArtistTopTracks() { return ToolsInternal.PrecacheArtistTopTracks(this); }
-    }
+		public int PrecacheArtistTopTracks() { return ToolsInternal.PrecacheArtistTopTracks(this); }
+	}
 }
