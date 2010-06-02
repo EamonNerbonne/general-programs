255a4f1bcd76251164a25b5fd706af940c89326e
eamon <devnull@localhost>
2010-06-02 07:41:55 +0000

    Using the previously expanded random matrix init methods.

--- LvqEmn/LvqLib/DataSetUtils.cpp
+++ LvqEmn/LvqLib/DataSetUtils.cpp
@@ -1,16 +1,21 @@
-#include "StdAfx.h"
+#include "stdafx.h"
+
 #include "DatasetUtils.h"
 
 MatrixXd DatasetUtils::MakePointCloud(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int pointCount, double meansep) {
-	MatrixXd P(dims, dims);
-	RandomMatrixInit(rngParams, P, 0, 1.0);
+	MatrixXd P = randomUnscalingMatrix<MatrixXd>(rngParams, dims);
+	boost::uniform_01<boost::mt19937> uniform01_rand(rngParams);
+	P*=exp(uniform01_rand()*2.0-1.0);
 
 	VectorXd offset(dims);
 	RandomMatrixInit(rngParams, offset, 0, meansep);
 
 	MatrixXd points(dims,pointCount);
+	  
 	RandomMatrixInit(rngInst, points, 0, 1.0);
-
+	cout<< "Norm pretransform: "<<projectionSquareNorm(points)<<"\n";
+	cout<< "Norm posttransform: "<<projectionSquareNorm(P * points)<<"\n";
+	
 	return P * points + offset * VectorXd::Ones(pointCount).transpose();
 }
 
@@ -30,14 +35,14 @@ LvqDataset* DatasetUtils::ConstructGaussianClouds(boost::mt19937 & rngParams, bo
 
 MatrixXd MakeTailMeans(boost::mt19937 & rndGen, int numStarTails, int starDim, double meansep) {
 	MatrixXd tailMeans(starDim,numStarTails);
-	DatasetUtils::RandomMatrixInit(rndGen,tailMeans,0,meansep);
+	RandomMatrixInit(rndGen,tailMeans,0,meansep);
 	return tailMeans;
 }
 vector<MatrixXd> MakeTailTransforms(boost::mt19937 & rndGen, int numStarTails, int starDim) {
 	vector<MatrixXd> tailTransforms;
 	for(int i=0;i<numStarTails;i++) {
 		MatrixXd t(starDim,starDim);
-		DatasetUtils::RandomMatrixInit(rndGen,t,0,1.0);
+		RandomMatrixInit(rndGen,t,0,1.0);
 		normalizeMatrix(t);
 		tailTransforms.push_back(t);
 	}
@@ -47,13 +52,16 @@ vector<MatrixXd> MakeTailTransforms(boost::mt19937 & rndGen, int numStarTails, i
 typedef boost::uniform_int<> starChoiceDistrib;
 typedef boost::variate_generator<boost::mt19937 &, starChoiceDistrib> starChoiceGen;
 
-LvqDataset* DatasetUtils::ConstructStarDataset(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int starDims, int numStarTails, int classCount,  int pointsPerClass,  double starMeanSep, double starClassRelOffset){
+LvqDataset* DatasetUtils::ConstructStarDataset(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int starDims, int numStarTails, int classCount,  int pointsPerClass,  double starMeanSep, double starClassRelOffset, bool randomlyRotate){
+	MatrixXd postInitTransform = randomOrthogonalMatrix<MatrixXd>(rngParams,dims);//always compute random transform, even if not needed, to ensure identical usage of random
+	if(!randomlyRotate) postInitTransform.setIdentity();
+
 	vector<MatrixXd> tailTransforms = MakeTailTransforms(rngParams, numStarTails, starDims);
 	MatrixXd tailMeans = MakeTailMeans(rngParams, numStarTails, starDims, starMeanSep);
 	
 	starChoiceGen starRndChoose(rngInst, starChoiceDistrib(0, numStarTails -1));
 
-	VectorXd starRaw(starDims);
+	VectorXd starRaw(starDims),fullPoint(dims);
 	MatrixXd points(dims, pointsPerClass * classCount);
 	vector<int> pointLabels(points.cols());
 	int pointIndex=0;
@@ -63,9 +71,12 @@ LvqDataset* DatasetUtils::ConstructStarDataset(boost::mt19937 & rngParams, boost
 		for(int i=0;i<pointsPerClass;++i) {
 			int starIdx = starRndChoose();
 			RandomMatrixInit(rngInst, starRaw, 0, 1.0);
-			points.block(0, pointIndex, starDims, 1) = currentTailMeans.col(starIdx) + tailTransforms[starIdx] * starRaw;
-			Eigen::Block<MatrixXd> restBlock(points.block(starDims, pointIndex, dims - starDims, 1));
+			
+			fullPoint.block(0,0,starDims,1)	= currentTailMeans.col(starIdx) + tailTransforms[starIdx] * starRaw;
+
+			Eigen::Block<VectorXd> restBlock(fullPoint.block(starDims, 0, dims - starDims, 1));
 			RandomMatrixInit(rngInst, restBlock,0,1.0);
+			points.block(0, pointIndex, dims, 1) = postInitTransform* fullPoint;
 			pointLabels[pointIndex] = label;
 			pointIndex++;
 		}
--- LvqEmn/LvqLib/DataSetUtils.h
+++ LvqEmn/LvqLib/DataSetUtils.h
@@ -9,16 +9,6 @@ class DatasetUtils
 public:
 	static Eigen::MatrixXd MakePointCloud(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int pointCount, double meansep);
 
-	template<typename T>static  void RandomMatrixInit(boost::mt19937 & rng, MatrixBase< T>& mat, double mean, double sigma) {
-		using namespace boost;
-		normal_distribution<> distrib(mean,sigma);
-		variate_generator<mt19937&, normal_distribution<> > rndGen(rng, distrib);
-
-		for(int j=0; j<mat.cols(); j++)
-			for(int i=0; i<mat.rows(); i++)
-				mat(i,j) = rndGen();
-	}
-
 	static LvqDataset* ConstructGaussianClouds(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int classCount, int pointsPerClass, double meansep);
-	static LvqDataset* ConstructStarDataset(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int starDims, int numStarTails,  int classCount, int pointsPerClass, double starMeanSep, double starClassRelOffset);
+	static LvqDataset* ConstructStarDataset(boost::mt19937 & rngParams, boost::mt19937 & rngInst, int dims, int starDims, int numStarTails,  int classCount, int pointsPerClass, double starMeanSep, double starClassRelOffset, bool randomlyRotate);
 };
