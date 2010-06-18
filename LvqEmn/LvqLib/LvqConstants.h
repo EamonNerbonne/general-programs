2dca3ef5d9e8090d6b15595fdb9c60c8bf431f19
eamon <devnull@localhost>
2010-06-18 09:34:15 +0000

    Better learning rates - it really helps!

--- LvqEmn/LvqLib/LvqConstants.h
+++ LvqEmn/LvqLib/LvqConstants.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "stdafx.h"
 
-#define LVQ_LrScaleP 0.1
-#define LVQ_LrScaleB 0.03
+#define LVQ_LrScaleP 0.03
+#define LVQ_LrScaleB 0.01
 #define LVQ_LrScaleBad 1.0
 #define LVQ_LR0 0.01
-#define LVQ_PERCLASSITERFACTOR 0.001
+#define LVQ_PERCLASSITERFACTOR 0.0002
