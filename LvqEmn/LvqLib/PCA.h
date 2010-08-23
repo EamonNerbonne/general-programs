a745774b37b92a1b8141b178d0f063dda740fdac
eamon <devnull@localhost>
2010-08-23 08:37:55 +0000

    Working on PCA

--- LvqEmn/LvqLib/PCA.h
+++ LvqEmn/LvqLib/PCA.h
@@ -2,26 +2,28 @@
 #include "stdafx.h"
 #include "Eigen/EigenValues"
 
-typedef Eigen::Matrix2d TMatrix;
-typedef PMatrix TPoints;
-typedef Eigen::Vector2d TPoint;
 
 
 //template <typename TPoints,typename TMatrix, typename TPoint>
 class PrincipalComponentAnalysis {
+typedef Eigen::Matrix2d TMatrix;
+typedef PMatrix TPoints;
+typedef Eigen::Vector2d TPoint;
 	
 public:
 	static TPoint MeanPoint(Eigen::MatrixBase<TPoints>const & points) {
 		return points.rowwise().sum() * (1.0/points.cols());
 	}
 	static TMatrix Covariance(Eigen::MatrixBase<TPoints>const & points, TPoint const & mean) {
-		TPoint diff = TPoint::Zero(points.rows());
-		TMatrix cov = TMatrix::Zero(points.rows(),points.rows());
-		for(int i=0;i<points.cols();++i) {
-			diff.noalias() = points.col(i) - mean;
-			cov.noalias() += diff * diff.transpose();
-		}
-		return cov * (1.0/(points.cols()-1.0));
+		return (points.colwise() - mean) * (points.colwise() - mean).transpose() * (1.0/(points.cols()-1.0));
+		//equivalent, possibly faster version:
+		//TPoint diff = TPoint::Zero(points.rows());
+		//TMatrix cov = TMatrix::Zero(points.rows(),points.rows());
+		//for(int i=0;i<points.cols();++i) {
+		//	diff.noalias() = points.col(i) - mean;
+		//	cov.noalias() += diff * diff.transpose();
+		//}
+		//return cov * (1.0/(points.cols()-1.0));
 	}
 
 
@@ -35,8 +37,9 @@ public:
 	static void DoPca(Eigen::MatrixBase<TPoints>const & points ) {
 		TPoint mean = MeanPoint(points);
 		TMatrix covarianceMatrix = Covariance(points, mean);
-
-		Eigen::EigenSolver<TMatrix> solver(covarianceMatrix, true);
+		
+		Eigen::SelfAdjointEigenSolver<TMatrix> eigenSolver(covarianceMatrix, true);
+		//Eigen::EigenSolver<TMatrix> solver(covarianceMatrix, true); //less efficient that specific function above.
 
 
 	}
