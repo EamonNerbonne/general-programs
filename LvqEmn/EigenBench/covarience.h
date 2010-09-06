f068bd9d5ee1e5dc52377017da2450408a14bcca
eamon <devnull@localhost>
2010-09-06 14:39:28 +0000

    Cleanup up covarience.h; now includes an extra manual implementation and it has a little easier output script.  Also fixed minor bug.

--- LvqEmn/EigenBench/covarience.h
+++ LvqEmn/EigenBench/covarience.h
@@ -16,8 +16,8 @@ using namespace Eigen;
 template<typename Scalar,int Size, int StorageOrder>
 EIGEN_DONT_INLINE void cov1(const Matrix<Scalar,Size,Dynamic,StorageOrder>& data, Matrix<Scalar,Size,Size,StorageOrder>& cov)
 {
-  Matrix<Scalar,Size,Dynamic,StorageOrder> tmp = data.colwise() - data.col(0);
-  cov.noalias() = tmp * tmp.adjoint();
+  //Matrix<Scalar,Size,Dynamic,StorageOrder> tmp = data.colwise() - data.col(0);
+  cov.noalias() = (data.colwise() - data.col(0)) * (data.colwise() - data.col(0)).adjoint();
 }
 
 template<typename Scalar,int Size, int StorageOrder>
@@ -25,16 +25,17 @@ EIGEN_DONT_INLINE void cov2(const Matrix<Scalar,Size,Dynamic,StorageOrder>& data
 {
   cov.setZero();
   Matrix<Scalar,Size,Dynamic,StorageOrder> tmp = data.colwise() - data.col(0);
-  cov.template selfadjointView<Lower>().rankUpdate(data);
+  cov.template selfadjointView<Lower>().rankUpdate(tmp);
 }
 
 template<typename Scalar,int Size, int StorageOrder>
 EIGEN_DONT_INLINE void cov3(const Matrix<Scalar,Size,Dynamic,StorageOrder>& data, Matrix<Scalar,Size,Size,StorageOrder>& cov)
 {
   cov.setZero();
+  Matrix<Scalar,Size,1> tmp;
   for(int i=0; i<data.cols(); ++i)
   {
-    Matrix<Scalar,Size,1> tmp = data.col(i) - data.col(0);
+    tmp = data.col(i) - data.col(0);
     cov.noalias() += tmp * tmp.adjoint();
   }
 }
@@ -43,9 +44,10 @@ template<typename Scalar,int Size, int StorageOrder>
 EIGEN_DONT_INLINE void cov4(const Matrix<Scalar,Size,Dynamic,StorageOrder>& data, Matrix<Scalar,Size,Size,StorageOrder>& cov)
 {
   cov.setZero();
+  Matrix<Scalar,Size,1> tmp;
   for(int i=0; i<data.cols(); ++i)
   {
-    Matrix<Scalar,Size,1> tmp = data.col(i) - data.col(0);
+    tmp = data.col(i) - data.col(0);
     cov.template selfadjointView<Lower>().rankUpdate(tmp);
   }
 }
@@ -54,14 +56,38 @@ template<typename Scalar,int Size, int StorageOrder>
 EIGEN_DONT_INLINE void cov5(const Matrix<Scalar,Size,Dynamic,StorageOrder>& data, Matrix<Scalar,Size,Size,StorageOrder>& cov)
 {
   cov.setZero();
+  Matrix<Scalar,Size,1> tmp;
   for(int i=0; i<data.cols(); ++i)
   {
-    Matrix<Scalar,Size,1> tmp = data.col(i) - data.col(0);
+    tmp = data.col(i) - data.col(0);
     cov.template triangularView<Lower>() += tmp * tmp.adjoint();
   }
 }
 
+template<typename Scalar,int Size, int StorageOrder>
+EIGEN_DONT_INLINE void cov6(const Matrix<Scalar,Size,Dynamic,StorageOrder>& data, Matrix<Scalar,Size,Size,StorageOrder>& cov)
+{
+  cov.setZero();
+  Matrix<Scalar,Size,1> tmp;
+  for(int i=0; i<data.cols(); ++i)
+  {
+    tmp = data.col(i) - data.col(0);
+	for(int j=0;j<cov.cols();++j)
+		for(int k=j;k<cov.cols();++k) 
+			cov.coeffRef(k,j) += tmp.coeff(j) *tmp.coeff(k);
+  }
+}
 
+#ifdef _MSC_VER
+#pragma warning (disable: 4127)
+#endif
+#define BENCHOUT(tries,repeats,code) \
+	do { \
+		BenchTimer BENCHOUT_timer; \
+		BENCH(BENCHOUT_timer, tries, repeats, code); \
+		std::cout<<BENCHOUT_timer.best()<<"s, "; \
+		break; \
+	} while(true)
 
 template<typename Scalar,int Size, int StorageOrder>
 void benchcov(int n, int size = Size)
@@ -75,42 +101,38 @@ void benchcov(int n, int size = Size)
   int repeats = int(10000000/(double(n)*size*size));
   repeats = repeats==0 ? 1 : repeats;
 
-  BenchTimer t1, t2, t3, t4, t5;
-  BENCH(t1, tries, repeats, (cov1< Scalar,Size,StorageOrder>(data,cov)));
-  BENCH(t2, tries, repeats, (cov2< Scalar,Size,StorageOrder>(data,cov)));
-  BENCH(t3, tries, repeats, (cov3< Scalar,Size,StorageOrder>(data,cov)));
-  BENCH(t4, tries, repeats, (cov4< Scalar,Size,StorageOrder>(data,cov)));
-  BENCH(t5, tries, repeats, (cov5< Scalar,Size,StorageOrder>(data,cov)));
+  std::cout << typeid(Scalar).name() << " " << Size << " " << size << " " << n << " => ";
+
+  BENCHOUT(tries, repeats, (cov1< Scalar,Size,StorageOrder>(data,cov)));
+  BENCHOUT(tries, repeats, (cov2< Scalar,Size,StorageOrder>(data,cov)));
+  BENCHOUT(tries, repeats, (cov3< Scalar,Size,StorageOrder>(data,cov)));
+  BENCHOUT(tries, repeats, (cov4< Scalar,Size,StorageOrder>(data,cov)));
+  BENCHOUT(tries, repeats, (cov5< Scalar,Size,StorageOrder>(data,cov)));
+  BENCHOUT(tries, repeats, (cov6< Scalar,Size,StorageOrder>(data,cov)));
   
-  std::cout << typeid(Scalar).name() << " " << Size << " " << size << " " << n
-            << " => " << t1.best()
-            << "s , " << t2.best()
-            << "s , " << t3.best()
-            << "s , " << t4.best()
-            << "s , " << t5.best()
-            << "s\n";
+  std::cout<<"\n";
 }
 
 typedef double scalarType;
 int docovbench()
 {
-  int n = 20;
+  int n = 2000;
   benchcov<scalarType,2,ColMajor>(n);
   benchcov<scalarType,3,ColMajor>(n);
   benchcov<scalarType,4,ColMajor>(n);
   benchcov<scalarType,Dynamic,ColMajor>(n,16);
   benchcov<scalarType,Dynamic,ColMajor>(n,128);
-  n = 200;
-  benchcov<scalarType,2,ColMajor>(n);
-  benchcov<scalarType,3,ColMajor>(n);
-  benchcov<scalarType,4,ColMajor>(n);
-  benchcov<scalarType,Dynamic,ColMajor>(n,16);
-  benchcov<scalarType,Dynamic,ColMajor>(n,128);
-  n = 2000;
-  benchcov<scalarType,2,ColMajor>(n);
-  benchcov<scalarType,3,ColMajor>(n);
-  benchcov<scalarType,4,ColMajor>(n);
-  benchcov<scalarType,Dynamic,ColMajor>(n,16);
-  benchcov<scalarType,Dynamic,ColMajor>(n,128);
+  //n = 200;
+  //benchcov<scalarType,2,ColMajor>(n);
+  //benchcov<scalarType,3,ColMajor>(n);
+  //benchcov<scalarType,4,ColMajor>(n);
+  //benchcov<scalarType,Dynamic,ColMajor>(n,16);
+  //benchcov<scalarType,Dynamic,ColMajor>(n,128);
+  //n = 20;
+  //benchcov<scalarType,2,ColMajor>(n);
+  //benchcov<scalarType,3,ColMajor>(n);
+  //benchcov<scalarType,4,ColMajor>(n);
+  //benchcov<scalarType,Dynamic,ColMajor>(n,16);
+  //benchcov<scalarType,Dynamic,ColMajor>(n,128);
   return 0;
 }
