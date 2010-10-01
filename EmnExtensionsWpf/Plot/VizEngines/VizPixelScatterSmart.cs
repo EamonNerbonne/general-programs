4853c00b048a1c104b038139a4d9fc51f7ef1f5a
eamon <devnull@localhost>
2010-10-01 12:57:50 +0000

    (minor) formatting, boolean expression simplification.

--- EmnExtensionsWpf/Plot/VizEngines/VizPixelScatterSmart.cs
+++ EmnExtensionsWpf/Plot/VizEngines/VizPixelScatterSmart.cs
@@ -5,23 +5,19 @@ using System.Text;
 using System.Windows;
 using System.Windows.Media;
 
-namespace EmnExtensions.Wpf.Plot.VizEngines
-{
-	public class VizPixelScatterSmart : PlotVizTransform<Point[], Point[]>, IVizPixelScatter
-	{
+namespace EmnExtensions.Wpf.Plot.VizEngines {
+	public class VizPixelScatterSmart : PlotVizTransform<Point[], Point[]>, IVizPixelScatter {
 		const int MaxPointsInStreamGeometry = 15000;
 
 		protected override Point[] TransformedData(Point[] inputData) { return inputData; }
 		IVizPixelScatter engine = new VizPixelScatterGeom();
 		protected override IVizEngine<Point[]> Implementation { get { return engine; } }
 
-		public override void DataChanged(Point[] newData)
-		{
+		public override void DataChanged(Point[] newData) {
 			bool useBmpPlot = newData != null && newData.Length > MaxPointsInStreamGeometry;
-			bool reconstructEngine = useBmpPlot && !(engine is VizPixelScatterBitmap) || !useBmpPlot && !(engine is VizPixelScatterGeom);
+			bool reconstructEngine = engine is VizPixelScatterBitmap != useBmpPlot;
 
-			if (reconstructEngine)
-			{
+			if (reconstructEngine) {
 				IVizPixelScatter newImplementation = useBmpPlot ? (IVizPixelScatter)new VizPixelScatterBitmap() : new VizPixelScatterGeom();
 				newImplementation.Owner = Owner;
 				newImplementation.CoverageRatio = CoverageRatio;
