faf1b82a2949b8419ced4dae28808446a98cd86d
eamon <devnull@localhost>
2010-10-20 08:23:22 +0000

    Added an UpdateSync class encapsulating update synchronization locking logic.

---
+++ LvqEmn/LvqGui/UpdateSync.cs
@@ -0,0 +1,8 @@
+﻿namespace LvqGui {
+	class UpdateSync {
+		readonly object syncUpdates = new object();
+		bool busy, updateQueued;
+		public bool UpdateEnqueue_IsMyTurn() { lock (syncUpdates) { updateQueued = busy; busy = true; return !updateQueued; } }
+		public bool UpdateDone_IsQueueEmpty() { lock (syncUpdates) { busy = false; return !updateQueued; } }
+	}
+}
