a27650c6793f6dbb15f37a9a33969de8605f2d9b
eamon <devnull@localhost>
2010-10-27 13:36:29 +0000

    Added klaas user

--- SongSearchLinq/SongSearchSite/logon.aspx.cs
+++ SongSearchLinq/SongSearchSite/logon.aspx.cs
@@ -20,7 +20,7 @@ namespace SongSearchSite {
 		}
 
 		static HashSet<string> users = new HashSet<string> {
-			"eamon","emn13","el_martian","auke","bart","juri"
+			"eamon","emn13","el_martian","auke","bart","juri","klaas"
 		};
 		protected bool MyCustomMethod(string strUsername, string strPassword) {
 			return users.Contains(strUsername) && strPassword == "music";
