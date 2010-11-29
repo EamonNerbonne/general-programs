7b274cfdcca60c48b46fffb644a8ce2dd19d56b4
eamon <devnull@localhost>
2010-11-29 08:53:23 +0000

    Better constants for faster prototype adaptation - but not too quick matrix adaptation.

--- LvqEmn/LvqLib/LvqConstants.h
+++ LvqEmn/LvqLib/LvqConstants.h
@@ -1,7 +1,7 @@
 #pragma once
-#define LVQ_LrScaleP 0.03
-#define LVQ_LrScaleB 0.01
-#define LVQ_LrScaleBad 0.1
-#define LVQ_LR0 0.01
-#define LVQ_ITERFACTOR_PERPROTO (1.0/20000.0)
+#define LVQ_LrScaleP 0.01
+#define LVQ_LrScaleB 0.003
+#define LVQ_LrScaleBad 1.0
+#define LVQ_LR0 0.03
+#define LVQ_ITERFACTOR_PERPROTO (1.0/5000.0)
 
