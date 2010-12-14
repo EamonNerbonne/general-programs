f2cd679cd5b7126d3f5cdbbee12238c884884254
eamon <devnull@localhost>
2010-12-14 11:07:12 +0000

    Fixed bug in EnsureLocalFilesInDB (SongRef might be null)

--- SongSearchLinq/LastFMspider/ToolsInternal/EnsureLocalFilesInDB.cs
+++ SongSearchLinq/LastFMspider/ToolsInternal/EnsureLocalFilesInDB.cs
@@ -8,17 +8,28 @@ namespace LastFMspider {
 			var LastFmCache = tools.LastFmCache;
 
 			Console.WriteLine("Loading song database...");
-			Console.WriteLine("Taking  {0} songs and indexing em by artist/title...", tools.SongFilesSearchData.songs.Length);
-			SongRef[] songsToDownload = tools.FindByName.Select(group => group.Key).ToArray();
+			Console.WriteLine("Taking  {0} songs putting artist/title into DB...", tools.SongFilesSearchData.songs.Length);
+			SongRef[] songsToDownload = tools.FindByName.Select(group => group.Key).OfType<SongRef>().ToArray();
 			LastFmCache.DoInLockedTransaction(() => {
-				foreach (SongRef songref in songsToDownload) {
-					try {
-						LastFmCache.InsertTrack.Execute(songref);
-					} catch (Exception e) {
-						Console.WriteLine("Exception: {0}", e);
-					}//ignore all errors.
-				}
+				LastFmCache.DoInLockedTransaction(() => {
+					int progressC = 0;
+					foreach (SongRef songref in songsToDownload) {
+						try {
+							LastFmCache.InsertTrack.Execute(songref);
+						} catch (Exception e) {
+							Console.WriteLine("Exception: {0}", e);
+						}//ignore all errors.
+						ProgressReport(progressC++, songsToDownload.Length);
+					}
+				});
 			});
+		}
+
+		private static void ProgressReport(int current, int count) {
+			if (current + 1 == count)
+				Console.WriteLine("done.");
+			else if ((current + 1) * 100L / count != current * 100L / count)
+				Console.Write((current + 1) * 100L / count + "% ");
 		}
 	}
 }
