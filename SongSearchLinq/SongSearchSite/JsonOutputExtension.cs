24befb5586dfc7f0986bfd8ef5ad7716a2f5b55a
eamon <devnull@localhost>
2010-12-16 07:39:35 +0000

    Added handy to-json extension method.

---
+++ SongSearchLinq/SongSearchSite/JsonOutputExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace SongSearchSite {
+	public static class JsonOutputExtension {
+		public static void JsonOutput(this HttpResponse response, object toSerialize) {
+			response.ContentType = "application/json";
+			response.Output.Write(JsonConvert.SerializeObject(toSerialize));
+		}
+	}
+}
