b6762f5484f9a7d1f67c0f2c05c3edf531f80cbf
eamon <devnull@localhost>
2010-12-16 07:49:09 +0000

    Enable initial attempt at song scrobbling; not yet finished.

---
+++ SongSearchLinq/SongSearchSite/ScrobbleSong.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Web;
+using Newtonsoft.Json;
+using System.Linq;
+using LastFMspider;
+using SongDataLib;
+using System.Collections.Generic;
+using System.Diagnostics;
+using LastFMspider.OldApi;
+
+namespace SongSearchSite {
+	public class ScrobbleSongHandler : IHttpHandler {
+		public bool IsReusable { get { return true; } }
+
+		class ScrobbleMessage {
+			// ReSharper disable UnaccessedField.Local
+#pragma warning disable 649
+			public string user, pass;
+			public string href, label;
+			public string status;
+#pragma warning restore 649
+			// ReSharper restore UnaccessedField.Local
+		}
+		static SongRef SongGuess(string label) {
+			if (label == null) return null;
+			int sepIdx = label.IndexOf(" - ");
+			if (sepIdx == -1) return null;
+			if (label.IndexOf(" - ", sepIdx + 1) != -1) return null;//ambiguous
+			return SongRef.Create(label.Substring(0, sepIdx), label.Substring(sepIdx + 3));
+		}
+
+		public void ProcessRequest(HttpContext context) {
+			try {
+				ScrobbleMessage command = JsonConvert.DeserializeObject<ScrobbleMessage>(context.Request["scrobbler"]);
+				var fullSongData = SongDbContainer.GetSongFromFullUri(Uri.UnescapeDataString(command.href)) as SongFileData;
+
+				SongRef songGuess = SongGuess(command.label);
+				bool haveSomeGuess = fullSongData != null || songGuess != null;
+				if (haveSomeGuess) {
+					var submitter = ScrobbleSubmitter.DoHandshake(command.user, command.pass);
+					if (submitter.IsOK) {
+						var ok = fullSongData != null
+							? submitter.SubmitNowPlaying(fullSongData)
+							: submitter.SubmitNowPlaying(songGuess);
+						context.Response.JsonOutput(new {
+							status = submitter.HandshakeStatus,
+							is_ok = submitter.IsOK,
+							completed = ok,
+						});
+					} else {
+						context.Response.JsonOutput(new {
+							status = submitter.HandshakeStatus,
+							is_ok = submitter.IsOK,
+							completed = false,
+						});
+					}
+				} else
+					context.Response.JsonOutput(new {
+						status = false,
+						is_ok = false,
+						completed = false,
+						songerror = true,
+					});
+
+			} catch (Exception e) {
+				context.Response.JsonOutput(new {
+					error = e.GetType().FullName,
+					message = e.Message,
+					fulltrace = e.ToString(),
+				});
+			}
+		}
+	}
+}
