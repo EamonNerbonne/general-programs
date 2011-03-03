9edd5c4e4c86e7e4f991603003c6bb5c84dd733e
eamon <devnull@localhost>
2011-03-03 15:20:33 +0000

    Don't track projection matrix for GGM; its constant due to normalization.

--- LvqEmn/LvqLib/LvqProjectionModel.cpp
+++ LvqEmn/LvqLib/LvqProjectionModel.cpp
@@ -3,15 +3,18 @@
 #include "LvqDataset.h"
 #include "utils.h"
 #include "RandomMatrix.h"
+#include "GgmLvqModel.h"
 void LvqProjectionModel::AppendTrainingStatNames(std::vector<std::wstring> & retval) const { 
 	LvqModel::AppendTrainingStatNames(retval);
-	retval.push_back(L"Projection Norm|norm|Projection Matrix");
+	if(!dynamic_cast<GgmLvqModel const*>(this))
+		retval.push_back(L"Projection Norm|norm|Projection Matrix");
 	if(settings.TrackProjectionQuality)
 		retval.push_back(L"Projected NN Error Rate|error rate|Projection Quality");
 }
 void LvqProjectionModel::AppendOtherStats(std::vector<double> & stats, LvqDataset const * trainingSet, std::vector<int>const & trainingSubset, LvqDataset const * testSet, std::vector<int>const & testSubset) const { 
 	LvqModel::AppendOtherStats(stats,trainingSet,trainingSubset,testSet,testSubset);
-	stats.push_back(projectionSquareNorm(P));
+	if(!dynamic_cast<GgmLvqModel const*>(this))
+		stats.push_back(projectionSquareNorm(P));
 	if(settings.TrackProjectionQuality)
 		stats.push_back(trainingSet ? trainingSet->NearestNeighborProjectedErrorRate(trainingSubset,testSet,testSubset,this->P) : 0.0);
 }
@@ -39,7 +42,6 @@ inline void randomProjectionMatrix(boost::mt19937 & rngParams, PMatrix & mat) {
 }
 
 
-
 LvqProjectionModel::LvqProjectionModel(LvqModelSettings & initSettings) 
 	: LvqModel(initSettings)
 	, P(LVQ_LOW_DIM_SPACE, initSettings.Dimensions()) 
@@ -48,6 +50,7 @@ LvqProjectionModel::LvqProjectionModel(LvqModelSettings & initSettings)
 		randomProjectionMatrix(initSettings.RngParams, P);
 	else
 		P = initSettings.pcaTransform();	//P.setIdentity();
+	normalizeProjection(P);
 }
 
 
