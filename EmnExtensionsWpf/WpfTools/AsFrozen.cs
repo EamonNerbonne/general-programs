e106c035aae851a3daf71b3828e06b4d4fe99280
eamon <devnull@localhost>
2011-03-03 08:37:16 +0000

    Added AsFrozen overload that gets to change the value before refreezing.

--- EmnExtensionsWpf/WpfTools/AsFrozen.cs
+++ EmnExtensionsWpf/WpfTools/AsFrozen.cs
@@ -1,9 +1,16 @@
 ﻿using System.Windows;
+using System;
 
 namespace EmnExtensions.Wpf {
 	public static partial class WpfTools {
 		public static T AsFrozen<T>(this T freezable) where T : Freezable {
 			return (T)freezable.GetAsFrozen(); 
+		}
+		public static T AsFrozen<T>(this T freezable,Action<T> changer) where T : Freezable {
+			T copy=(T)freezable.CloneCurrentValue();
+			changer(copy);
+			copy.Freeze();
+			return copy;
 		}
 	}
 }
