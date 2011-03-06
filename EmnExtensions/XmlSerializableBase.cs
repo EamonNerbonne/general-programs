05b59f07ff53605a170ddf6aa0ae2cea0c595c92
eamon <devnull@localhost>
2011-03-06 00:03:31 +0000

    Added handy overload.

--- EmnExtensions/XmlSerializableBase.cs
+++ EmnExtensions/XmlSerializableBase.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Xml;
-using System.Xml.Serialization;
-using System.IO;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
-namespace EmnExtensions
-{
-	public class XmlSerializableBase<T> where T : XmlSerializableBase<T>
-	{
-		static XmlSerializer serializer = new XmlSerializer(typeof(T));
+namespace EmnExtensions {
+	public class XmlSerializableBase<T> where T : XmlSerializableBase<T> {
+		static readonly XmlSerializer serializer = new XmlSerializer(typeof(T));
 		public static T Deserialize(XmlReader from) { return (T)serializer.Deserialize(from); }
+		public static T Deserialize(XDocument from) { using (var reader = from.CreateReader()) return Deserialize(reader); }
 		public void SerializeTo(Stream s) { serializer.Serialize(s, this); }
 		public void SerializeTo(TextWriter w) { serializer.Serialize(w, this); }
 		public void SerializeTo(XmlWriter xw) { serializer.Serialize(xw, this); }
