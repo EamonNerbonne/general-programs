739c9dcad0a3a9e1e519205b1107c7b390e7fbf1
eamon <devnull@localhost>
2011-04-20 11:30:32 +0000

    Added Handy IndexOfExtension.cs to find items in a list.

---
+++ EmnExtensions/Enumerables/IndexOfExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmnExtensions {
+	public static class IndexOfExtension {
+		public static int IndexOf<T>(this IEnumerable<T> list, Func<T, bool> pred) {
+			int index = 0;
+			foreach (var item in list) {
+				if (pred(item))
+					return index;
+				index++;
+			}
+			return -1;
+		}
+	}
+}
