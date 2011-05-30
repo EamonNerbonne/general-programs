962994af7ed72e23f233287b4411f4872b1af5fe
eamon <devnull@localhost>
2011-05-30 12:26:14 +0000

    clean up usings

--- SongSearchLinq/SongData/Config/AbstractSongDataConfigSection.cs
+++ SongSearchLinq/SongData/Config/AbstractSongDataConfigSection.cs
@@ -1,10 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
 namespace SongDataLib {
 
 	public abstract class AbstractSongDataConfigSection : ISongDataConfigSection {
