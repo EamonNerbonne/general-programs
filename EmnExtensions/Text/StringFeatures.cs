273ba9e7b772f0085fbd1f5cd6f7c185ca96a28f
eamon <devnull@localhost>
2011-06-06 13:42:27 +0000

    Add SubstringAfter method to string.

--- EmnExtensions/Text/StringFeatures.cs
+++ EmnExtensions/Text/StringFeatures.cs
@@ -1,8 +1,10 @@
 ﻿
-namespace EmnExtensions.Text
-{
-	public static class StringFeatures
-	{
+namespace EmnExtensions.Text {
+	public static class StringFeatures {
 		public static bool IsNullOrEmpty(this string str) { return string.IsNullOrEmpty(str); }
+		public static string SubstringAfter(this string haystack, string needle) {
+			int needleIdx = haystack.IndexOf(needle);
+			return needleIdx == -1 ? null : haystack.Substring(needleIdx + needle.Length);
+		}
 	}
 }
