e8793c46dd1b24ed9b6c1fbb5ca207f2f400ee2c
eamon <devnull@localhost>
2011-06-06 17:46:34 +0000

    Add method to parse results text file and load optimal results from that.

--- LvqEmn/LvqGui/DatasetResults.cs
+++ LvqEmn/LvqGui/DatasetResults.cs
@@ -4,15 +4,43 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using EmnExtensions.Algorithms;
+using EmnExtensions.Text;
 using LvqLibCli;
 
 namespace LvqGui {
 	public class DatasetResults {
-		public readonly FileInfo resultFile;
-		public readonly double iters;
-		public readonly LvqModelSettingsCli resSettings;
-		DatasetResults(FileInfo fi, double iters, LvqModelSettingsCli settings) { resSettings = settings; this.iters = iters; resultFile = fi; }
+		public readonly FileInfo resultsFile;
+		public readonly double trainedIterations;
+		public readonly LvqModelSettingsCli unoptimizedSettings;
+		DatasetResults(FileInfo fi, double iters, LvqModelSettingsCli settings) { unoptimizedSettings = settings; trainedIterations = iters; resultsFile = fi; }
 
+		public LvqModelSettingsCli GetOptimizedSettings() {
+			string[] fileLines = File.ReadAllLines(resultsFile.FullName);
+			double[] lr0range = ExtractLrs(fileLines.First(line => line.StartsWith("lr0range:")));
+			double[] lrPrange = ExtractLrs(fileLines.First(line => line.StartsWith("lrPrange:")));
+			double[] lrBrange = ExtractLrs(fileLines.First(line => line.StartsWith("lrBrange:")));
+			string[] resultLines = fileLines.SkipWhile(line => !line.StartsWith(".")).Skip(1).TakeWhile(line => !line.StartsWith("Search Complete!")).ToArray();
+			double[] lrOfBestResult = resultLines.First().SubstringBefore(":").Split('p', 'b').Select(double.Parse).ToArray();
+
+			double lr0 = ClosestMatch(lr0range, lrOfBestResult[0]);
+			double lrp = ClosestMatch(lrPrange, lrOfBestResult[1]);
+			double lrb = ClosestMatch(lrBrange, lrOfBestResult[2]);
+			LvqModelSettingsCli retval = unoptimizedSettings.Copy();
+			retval.LR0 = lr0;
+			retval.LrScaleP = lrp;
+			retval.LrScaleB = lrb;
+			return retval;
+		}
+
+		static readonly char[] comma = new[] { ',' };
+		static double[] ExtractLrs(string line) {
+			return line.SubstringAfter("{").SubstringBefore("}").Split(comma).Select(double.Parse).ToArray();
+		}
+		static double ClosestMatch(IEnumerable<double> haystack, double needle) {
+			return haystack.Aggregate(new { Err = double.PositiveInfinity, Val = needle },
+				(best, option) => Math.Abs(option - needle) < best.Err ? new { Err = Math.Abs(option - needle), Val = option } : best).Val;
+		}
+		
 		static readonly Regex resultsFilenameRegex = new Regex(@"^(?<iters>[0-9]?e[0-9])+\-(?<shorthand>[^ ]*?)( \([0-9+]\))?\.txt$");
 
 		static IEnumerable<DatasetResults> FromDataset(LvqDatasetCli dataset) {
@@ -39,8 +67,8 @@ namespace LvqGui {
 
 			var matchingFiles =
 				from result in FromDataset(dataset)
-				where WithoutLrOrSeeds(result.resSettings).ToShorthand() == lrIgnoredSettings.ToShorthand()
-				orderby result.iters descending
+				where WithoutLrOrSeeds(result.unoptimizedSettings).ToShorthand() == lrIgnoredSettings.ToShorthand()
+				orderby result.trainedIterations descending
 				select result;
 
 			return matchingFiles.FirstOrDefault();
@@ -54,12 +82,12 @@ namespace LvqGui {
 
 			var matchingFiles =
 				from result in FromDataset(dataset)
-				where WithoutModelAndPrototypes(WithoutLrOrSeeds(result.resSettings)).ToShorthand() == modelIgnoredSettings.ToShorthand()
-				group result by result.iters into resGroup
-				where resGroup.Select(res => new { res.resSettings.ModelType, res.resSettings.PrototypesPerClass })
+				where WithoutModelAndPrototypes(WithoutLrOrSeeds(result.unoptimizedSettings)).ToShorthand() == modelIgnoredSettings.ToShorthand()
+				group result by result.trainedIterations into resGroup
+				where resGroup.Select(res => new { res.unoptimizedSettings.ModelType, res.unoptimizedSettings.PrototypesPerClass })
 												.SetEquals(TestLr.ModelTypes.SelectMany(mt => TestLr.PrototypesPerClassOpts.Select(ppc => new { ModelType = mt, PrototypesPerClass = ppc })))
 				orderby resGroup.Key descending
-				select resGroup;
+				select resGroup.ToArray();
 
 			return matchingFiles.FirstOrDefault();
 		}
