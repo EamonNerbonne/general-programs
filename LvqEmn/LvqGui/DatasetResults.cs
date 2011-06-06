79b00f6a6004bc9c1b1a2eb5c41529b7affc0fc1
eamon <devnull@localhost>
2011-06-06 15:57:46 +0000

    Added some comments and a public static method accessible from linqpad to DatasetResults that allows loading from a specific file.

--- LvqEmn/LvqGui/DatasetResults.cs
+++ LvqEmn/LvqGui/DatasetResults.cs
@@ -6,9 +6,8 @@ using System.Text.RegularExpressions;
 using EmnExtensions.Algorithms;
 using LvqLibCli;
 
-namespace LvqGui
-{
-	class DatasetResults {
+namespace LvqGui {
+	public class DatasetResults {
 		public readonly FileInfo resultFile;
 		public readonly double iters;
 		public readonly LvqModelSettingsCli resSettings;
@@ -16,37 +15,49 @@ namespace LvqGui
 
 		static readonly Regex resultsFilenameRegex = new Regex(@"^(?<iters>[0-9]?e[0-9])+\-(?<shorthand>[^ ]*?)( \([0-9+]\))?\.txt$");
 
-		public static IEnumerable<DatasetResults> FromDataset(LvqDatasetCli dataset) {
+		static IEnumerable<DatasetResults> FromDataset(LvqDatasetCli dataset) {
 			DirectoryInfo datasetResultsDir = GetDatasetResultDir(dataset);
 			if (datasetResultsDir == null) return Enumerable.Empty<DatasetResults>();
 			return
 				from resultFile in datasetResultsDir.GetFiles("*.txt")
-				let match = resultsFilenameRegex.Match(resultFile.Name)
-				where match.Success
-				select new DatasetResults(resultFile, Double.Parse(match.Groups["iters"].Value), WithoutLrOrSeeds(CreateLvqModelValues.SettingsFromShorthand(match.Groups["shorthand"].Value)));
+				let parsedResults = ProcFile(resultFile)
+				where parsedResults != null
+				select parsedResults;
 		}
 
+		public static DatasetResults ProcFile(FileInfo resultFile) {
+			var match = resultsFilenameRegex.Match(resultFile.Name);
+			if (!match.Success) return null;
+
+			return new DatasetResults(resultFile, Double.Parse(match.Groups["iters"].Value.StartsWith("e") ? "1" + match.Groups["iters"].Value : match.Groups["iters"].Value), CreateLvqModelValues.SettingsFromShorthand(match.Groups["shorthand"].Value));
+		}
+		/// <summary>
+		/// Gets the lr-optimized result for the given dataset and settings with the largest number of iterations, or null if no results have been done for this settings+dataset combination.
+		/// </summary>
 		public static DatasetResults GetBestResult(LvqDatasetCli dataset, LvqModelSettingsCli settings) {
 			var lrIgnoredSettings = WithoutLrOrSeeds(settings);
 
 			var matchingFiles =
 				from result in FromDataset(dataset)
-				where result.resSettings.ToShorthand() == lrIgnoredSettings.ToShorthand()
+				where WithoutLrOrSeeds(result.resSettings).ToShorthand() == lrIgnoredSettings.ToShorthand()
 				orderby result.iters descending
 				select result;
 
 			return matchingFiles.FirstOrDefault();
 		}
 
+		/// <summary>
+		/// Gets the set of dataset lr-optimized results for the given dataset and settings with the largest number of iterations, or null if not all modeltype/prototype combos are done.
+		/// </summary>
 		public static IEnumerable<DatasetResults> GetBestResults(LvqDatasetCli dataset, LvqModelSettingsCli settings) {
 			var modelIgnoredSettings = WithoutModelAndPrototypes(WithoutLrOrSeeds(settings));
 
 			var matchingFiles =
 				from result in FromDataset(dataset)
-				where WithoutModelAndPrototypes(result.resSettings).ToShorthand() == modelIgnoredSettings.ToShorthand()
+				where WithoutModelAndPrototypes(WithoutLrOrSeeds(result.resSettings)).ToShorthand() == modelIgnoredSettings.ToShorthand()
 				group result by result.iters into resGroup
 				where resGroup.Select(res => new { res.resSettings.ModelType, res.resSettings.PrototypesPerClass })
-				                             	.SetEquals(TestLr.ModelTypes.SelectMany(mt => TestLr.PrototypesPerClassOpts.Select(ppc => new { ModelType = mt, PrototypesPerClass = ppc })))
+												.SetEquals(TestLr.ModelTypes.SelectMany(mt => TestLr.PrototypesPerClassOpts.Select(ppc => new { ModelType = mt, PrototypesPerClass = ppc })))
 				orderby resGroup.Key descending
 				select resGroup;
 
