ddc5fd2b2328fdc80b4ce3eb3059f85eb8ba6bbd
eamon <devnull@localhost>
2011-06-08 20:41:27 +0000

    When getting optimized results to load learning rates, look for results for any dataset that's identical down to modifications to the instantiation seed (i.e. same shape, but different order if loaded, and different details if generated).

--- LvqEmn/LvqGui/DatasetResults.cs
+++ LvqEmn/LvqGui/DatasetResults.cs
@@ -14,7 +14,7 @@ namespace LvqGui {
 		public readonly LvqModelSettingsCli unoptimizedSettings;
 		DatasetResults(FileInfo fi, double iters, LvqModelSettingsCli settings) { unoptimizedSettings = settings; trainedIterations = iters; resultsFile = fi; }
 
-		public LvqModelSettingsCli GetOptimizedSettings() {
+		public LvqModelSettingsCli GetOptimizedSettings(uint? paramSeed = null, uint? instSeed = null) {
 			string[] fileLines = File.ReadAllLines(resultsFile.FullName);
 			double[] lr0range = ExtractLrs(fileLines.First(line => line.StartsWith("lr0range:")));
 			double[] lrPrange = ExtractLrs(fileLines.First(line => line.StartsWith("lrPrange:")));
@@ -29,6 +29,8 @@ namespace LvqGui {
 			retval.LR0 = lr0;
 			retval.LrScaleP = lrp;
 			retval.LrScaleB = lrb;
+			retval.ParamsSeed = paramSeed ?? retval.ParamsSeed;
+			retval.InstanceSeed = instSeed ?? retval.InstanceSeed;
 			return retval;
 		}
 
@@ -40,13 +42,12 @@ namespace LvqGui {
 			return haystack.Aggregate(new { Err = double.PositiveInfinity, Val = needle },
 				(best, option) => Math.Abs(option - needle) < best.Err ? new { Err = Math.Abs(option - needle), Val = option } : best).Val;
 		}
-		
+
 		static readonly Regex resultsFilenameRegex = new Regex(@"^(?<iters>[0-9]?e[0-9])+\-(?<shorthand>[^ ]*?)( \([0-9+]\))?\.txt$");
 
 		static IEnumerable<DatasetResults> FromDataset(LvqDatasetCli dataset) {
-			DirectoryInfo datasetResultsDir = GetDatasetResultDir(dataset);
-			if (datasetResultsDir == null) return Enumerable.Empty<DatasetResults>();
 			return
+				from datasetResultsDir in GetDatasetResultDir(dataset)
 				from resultFile in datasetResultsDir.GetFiles("*.txt")
 				let parsedResults = ProcFile(resultFile)
 				where parsedResults != null
@@ -68,7 +69,7 @@ namespace LvqGui {
 			var matchingFiles =
 				from result in FromDataset(dataset)
 				where WithoutLrOrSeeds(result.unoptimizedSettings).ToShorthand() == lrIgnoredSettings.ToShorthand()
-				orderby result.trainedIterations descending
+				orderby result.trainedIterations descending, dataset.DatasetLabel == result.resultsFile.Directory.Name descending
 				select result;
 
 			return matchingFiles.FirstOrDefault();
@@ -77,16 +78,16 @@ namespace LvqGui {
 		/// <summary>
 		/// Gets the set of dataset lr-optimized results for the given dataset and settings with the largest number of iterations, or null if not all modeltype/prototype combos are done.
 		/// </summary>
-		public static IEnumerable<DatasetResults> GetBestResults(LvqDatasetCli dataset, LvqModelSettingsCli settings) {
+		public static DatasetResults[] GetBestResults(LvqDatasetCli dataset, LvqModelSettingsCli settings) {
 			var modelIgnoredSettings = WithoutModelAndPrototypes(WithoutLrOrSeeds(settings));
 
 			var matchingFiles =
 				from result in FromDataset(dataset)
 				where WithoutModelAndPrototypes(WithoutLrOrSeeds(result.unoptimizedSettings)).ToShorthand() == modelIgnoredSettings.ToShorthand()
-				group result by result.trainedIterations into resGroup
+				group result by Tuple.Create(result.trainedIterations,result.resultsFile.Directory.Name) into resGroup
 				where resGroup.Select(res => new { res.unoptimizedSettings.ModelType, res.unoptimizedSettings.PrototypesPerClass })
 												.SetEquals(TestLr.ModelTypes.SelectMany(mt => TestLr.PrototypesPerClassOpts.Select(ppc => new { ModelType = mt, PrototypesPerClass = ppc })))
-				orderby resGroup.Key descending
+				orderby resGroup.Key.Item1 descending, resGroup.Key.Item2 == dataset.DatasetLabel descending
 				select resGroup.ToArray();
 
 			return matchingFiles.FirstOrDefault();
@@ -108,9 +109,19 @@ namespace LvqGui {
 			return retval;
 		}
 
-		static DirectoryInfo GetDatasetResultDir(LvqDatasetCli dataset) {
-			if (dataset == null) return null;
-			return TestLr.resultsDir.GetDirectories(dataset.DatasetLabel).FirstOrDefault();
+		static readonly Regex labelRegex = new Regex(@"^(?<prefix>.*?[,\[])(?<instseed>[0-9A-Fa-f]+)(?<suffix>\].*)$");
+		static Tuple<string, string, string> splitLabel(string label) {
+			Match m = labelRegex.Match(label);
+			if (!m.Success) return null;
+			return Tuple.Create(m.Groups["prefix"].Value, m.Groups["instseed"].Value, m.Groups["suffix"].Value);
+		}
+
+		static IEnumerable<DirectoryInfo> GetDatasetResultDir(LvqDatasetCli dataset) {
+			if (dataset == null) return Enumerable.Empty<DirectoryInfo>();
+			var split = splitLabel(dataset.DatasetLabel);
+
+			return TestLr.resultsDir.GetDirectories(split.Item1 + "*" + split.Item3)
+				.Where(di => { var diSplit = splitLabel(di.Name); return diSplit.Item1 == split.Item1 && diSplit.Item3 == split.Item3; });
 		}
 	}
 }
