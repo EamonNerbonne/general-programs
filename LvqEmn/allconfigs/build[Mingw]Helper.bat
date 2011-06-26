8caef0fd699d5245317e47a494251861ed591c2a
Eamon Nerbonne <eamon@nerbonne.org>
2011-06-26 12:54:41 +0200

    Get cmake to work within Visual Studio; can now use gcc compiled lib in lvqgui

--- LvqEmn/allconfigs/build[Mingw]Helper.bat
+++ LvqEmn/allconfigs/build[Mingw]Helper.bat
@@ -3,7 +3,7 @@ mingw32-make -j >%~dp0\_Build-mingw.log
 IF ERRORLEVEL 1 (
 	echo buildFailed > "%~dp0\_BuildFailed-mingw"
 ) ELSE (
-	del "%~dp0\_BuildFailed-mingw"
+	IF EXIST "%~dp0\_BuildFailed-mingw" (del "%~dp0\_BuildFailed-mingw")
 	IF EXIST "%~1" (
 		"%~1" %2 %3 %4 %5 %6 %7 %8 %9
 	)
