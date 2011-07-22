283f27f349018d5145230e5b4fed38ab676cbde1
Eamon Nerbonne <eamon@nerbonne.org>
2011-07-22 14:56:53 +0200

    when printing mean+stderr, allow control of fractional significant digits

--- EmnExtensions/MathHelpers/Statistics.cs
+++ EmnExtensions/MathHelpers/Statistics.cs
@@ -12,20 +12,22 @@ namespace EmnExtensions.MathHelpers
     /// </summary>
     public class Statistics
     {
-		public static string GetFormatted(double mean, double stderr, int extraprecision = 0, bool suppressStderr=false) {
+		public static string GetFormatted(double mean, double stderr, double extraprecision = 0, bool suppressStderr=false) {
 			string numF, errF;
 			if (stderr == 0 || double.IsNaN(stderr) || double.IsInfinity(stderr)) {
 				numF = "g";
 				errF = stderr == 0?"~ 0":"";
 			} else if (Math.Abs(mean) > 0 && Math.Abs(Math.Log10(Math.Abs(mean))) < 5) {
 				//use fixed-point:
-				int errOOM = Math.Max(0, (int)(1.5 - Math.Log10(stderr))) + extraprecision;
+				int errOOM = Math.Max(0, (int)(1.5 - Math.Log10(stderr) + extraprecision));
 				numF = "f" + errOOM;
 				errF = " ~ {1:f" + errOOM + "}";
 			} else {
-				int digits = Math.Abs(mean) <= stderr ? 1
-								: (int)(Math.Log10(Math.Abs(mean) / stderr) + 1.5);
-				numF = "g" + (digits + extraprecision);
+				double digitEstimate = Math.Abs(mean) <= stderr 
+								? 1.0
+								: (Math.Log10(Math.Abs(mean) / stderr) + 1.5);
+				int digits = (int)(digitEstimate + extraprecision);
+				numF = "g" + digits;
 				errF = " ~ {1:g2}";
 			}
 			return string.Format("{0:" + numF + "}" + (suppressStderr?"":errF), mean, stderr);
