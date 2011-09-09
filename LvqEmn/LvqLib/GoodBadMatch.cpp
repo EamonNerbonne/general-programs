6b9ccc671a5d2200187d49957f5f3ac9e650f1fa
Eamon Nerbonne <eamon@nerbonne.org>
2011-09-09 13:04:42 +0200

    Experiment with alternate cost functions.

--- LvqEmn/LvqLib/GoodBadMatch.cpp
+++ LvqEmn/LvqLib/GoodBadMatch.cpp
@@ -15,11 +15,37 @@ using std::tanh;
 	MatchQuality GoodBadMatch::GgmQuality() {
 		MatchQuality retval;
 		retval.isErr = distGood >= distBad;
+
 		retval.costFunc=tanh((distGood-distBad)/4.0);
 		retval.distBad = distBad;
 		retval.distGood = distGood;
 		retval.muJ = (1.0/4.0) * (1 - sqr(retval.costFunc));//
 		retval.muK = -retval.muJ;
 		return retval;
+
+		//double pGood = exp(-0.5*distGood);
+		//double pBad = exp(-0.5*distBad);
+		//retval.costFunc= pBad - pGood;
+		//retval.distBad = distBad;
+		//retval.distGood = distGood;
+		//retval.muJ = pGood;//
+		//retval.muK = -pBad;
+		//return retval;
+
+		//retval.costFunc=  distGood - distBad;
+		//retval.distBad = distBad;
+		//retval.distGood = distGood;
+		//retval.muJ = 1;//
+		//retval.muK = -1;
+		//return retval;
+
+		//double rawcost=1+tanh((distGood-distBad)/4.0);
+		//retval.costFunc=log(rawcost);
+		//retval.distBad = distBad;
+		//retval.distGood = distGood;
+		//retval.muJ = (1.0/4.0) * (1 - sqr(retval.costFunc)) / rawcost;//
+		//retval.muK = -retval.muJ;
+		//return retval;
+
 	}
 	
