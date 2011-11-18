e3f0e9f6a23b562ae9e14dcffddd578f1be3539e
Eamon Nerbonne <eamon@nerbonne.org>
2011-11-18 16:26:04 +0100

    Added comment concerning squaredNorm vs. norm: use squaredNorm.

--- LvqEmn/LvqLib/LgmLvqModel.cpp
+++ LvqEmn/LvqLib/LgmLvqModel.cpp
@@ -152,7 +152,7 @@ Matrix_NN LgmLvqModel::PrototypeDistances(Matrix_NN const & points) const {
 	for(size_t protoI=0;protoI<prototype.size();++protoI) {
 		tmpPointsDiff.noalias() = points.colwise() - prototype[protoI];
 		tmpPointsDiffProj.noalias() = P[protoI] * tmpPointsDiff;
-		newPoints.row(protoI).noalias() = (tmpPointsDiffProj.colwise().norm().array() + std::numeric_limits<double>::min()).log().matrix();//not squaredNorm?
+		newPoints.row(protoI).noalias() = (tmpPointsDiffProj.colwise().norm().array() + std::numeric_limits<double>::min()).log().matrix();//not squaredNorm? is never going to matter, so I should use (faster) squaredNorm... oh well.
 	}
 	return newPoints;
 }
