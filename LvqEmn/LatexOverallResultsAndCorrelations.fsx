5ef77b12acd51a989f8fac51899010932fade29d
Eamon Nerbonne <eamon@nerbonne.org>
2012-02-03 16:47:14 +0100

    removed unused variable; rename file to match function.

--- LvqEmn/LatexOverallResultsAndCorrelations.fsx
+++ LvqEmn/LatexOverallResultsAndCorrelations.fsx
@@ -7,7 +7,6 @@ open LvqLibCli
 open LvqGui    
 
 let allResults = LrOptResults.loadDatasetLrOptResults "base"
-let tenResults = List.filter (fun (result:DatasetResults) ->result.unoptimizedSettings.InstanceSeed < 20u) allResults
 
 LatexOverallErrorTables.lvqMethodsOptimalLrErrorsTable "base" allResults GeneralSettings.allTypesWithName |> printfn "%s"
 
