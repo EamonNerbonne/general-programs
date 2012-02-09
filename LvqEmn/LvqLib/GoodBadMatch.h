fd10830b7c9252452e7e6dbe3260e0cceea7fb24
Eamon Nerbonne <eamon@nerbonne.org>
2012-02-09 23:11:28 +0100

    GoodBadMatch.h: accept prototype if it's  NOT further away to ensure a match even if everything's NaN.  Also, extra asserts to assert sanity & warning fix (initialization order differs from initializer order)

--- LvqEmn/LvqLib/GoodBadMatch.h
+++ LvqEmn/LvqLib/GoodBadMatch.h
@@ -44,15 +44,15 @@ struct CorrectAndWorstMatches {
 
 	inline CorrectAndWorstMatches(MatchOk*matchesOk)
 		: distBad(std::numeric_limits<double>::infinity())
-		, matchesOk(matchesOk)
-		, foundOk(0)
 #ifndef NDEBUG
 		, matchBad(-1)
 #endif
+		, matchesOk(matchesOk)
+		, foundOk(0)
 	{}
 
 	inline void RegisterOk(double dist,int idx) { matchesOk[foundOk++] = MatchOk(dist,idx); }
-	inline void RegisterBad(double dist,int idx) { if(dist < distBad) { matchBad = idx; distBad = dist; } }
+	inline void RegisterBad(double dist,int idx) { if(!(dist >= distBad)) { matchBad = idx; distBad = dist; } }
 
 	inline void Register(double dist,int idx, bool isOk) { if(isOk) RegisterOk(dist,idx); else RegisterBad(dist,idx); }
 
@@ -64,6 +64,8 @@ struct CorrectAndWorstMatches {
 		retval.matchBad = matchBad;
 		retval.distGood = matchesOk[0].dist;
 		retval.matchGood = matchesOk[0].idx;
+		assert(retval.matchGood>=0);
+		assert(retval.matchBad>=0);
 		return retval;
 	}		
 };
