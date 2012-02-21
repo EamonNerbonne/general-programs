2e731bdc2feb5d468acf719ab2bfbe2390fb1536
Eamon Nerbonne <eamon@nerbonne.org>
2012-02-21 15:49:35 +0100

    Lgm, Lpq: support Ppca (allow selecting it too)

--- LvqEmn/LvqLibCli/LvqModelSettingsCli.cpp
+++ LvqEmn/LvqLibCli/LvqModelSettingsCli.cpp
@@ -40,7 +40,7 @@ namespace LvqLibCli {
 		
 		retval.noKP = retval.noKP && hasGlobalP;
 		retval.Popt =  retval.Popt && hasGlobalP;
-		retval.Ppca = retval.Ppca && hasGlobalP;
+		//retval.Ppca = retval.Ppca && (hasGlobalP || );
 		retval.NoNnErrorRateTracking = retval.NoNnErrorRateTracking && hasGlobalP;
 		retval.NGu = retval.NGu && PrototypesPerClass > 1 && hasGlobalP;
 		retval.NGi =retval.NGi && PrototypesPerClass > 1;
