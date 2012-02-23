04a9c854cb6727a3c397e7a1f16e8e4f7c65ca77
Eamon Nerbonne <eamon@nerbonne.org>
2012-02-23 17:04:51 +0100

    make shuffle truly unbiased

--- LvqEmn/LvqLib/shuffle.h
+++ LvqEmn/LvqLib/shuffle.h
@@ -3,8 +3,19 @@
 
 template<typename arrayT>
 void shuffle(boost::mt19937 & randGen, arrayT& arr, size_t size){
-	for(size_t i = 0; i<size;++i)
-		std::swap(arr[i],arr[i+randGen() %(size-i)]);
+	for(size_t i = 0; i<size;++i) {
+		unsigned range = size-i;
+		unsigned capped;
+		while(true) {
+			unsigned randNum = randGen();
+			capped = randNum % range;
+			// size-i might not divide 2^32 evenly; so reject rolls that are too high; they're too high if they're in the highest, incomplete bucket
+			//if the bucket was incomplete; then I can't add range - 1 - capped without overflowing.
+			// so if I overflow; try again
+			if(randNum <= randNum + (range - 1 - capped)) break;
+		}
+		std::swap(arr[i],arr [ i + capped]);
+	}
 }
 // (Slower) alternative is something like:
 //	random_shuffle(start, end, shuffle_rnd_helper(randGen) );
