27b82659946ca38e801e7657b9b1b9640a396c67
Eamon Nerbonne <eamon@nerbonne.org>
2012-08-08 16:38:21 +0200

    Edited project/cmake files to use VS2012 over vs2010

--- LvqEmn/allconfigs/build[Msc].bat
+++ LvqEmn/allconfigs/build[Msc].bat
@@ -4,7 +4,7 @@ set BASEDIR=%~dp0
 mkdir %~dp0\..\build\msc
 cd %~dp0\..\build\msc
 
-cmake -G "Visual Studio 10 Win64" "%BASEDIR%."
+cmake -G "Visual Studio 11 Win64" "%BASEDIR%."
 REM start "MsBuild" /low  /B /wait 
 timethis "%BASEDIR%\build[Msc]Helper.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9 >>"%BASEDIR%\_BuildTime-msc.log"
 chdir /d %OLDDIR% &rem restore current directory
