a5804b518c50fcef55e5f06f55fd51b37a58da5a
Eamon Nerbonne <eamon@nerbonne.org>
2013-02-01 17:49:25 +0100

    Start a third experiment, this one hopefully with a little simpler types.

---
+++ CsHtmlDsl_HtmlGenerator/HElemLambaGen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlGenerator
+{
+	struct HElemLambaGen
+	{
+		internal string Name;
+		internal SList<HAttr> Attrs;
+		internal SList<HNode> Kids;
+
+		public HElemLambaGen Append(HNode node) { return new HElemLambaGen { Name = Name, Attrs = Attrs, Kids = Kids.Prepend(node) }; }
+		public HElemLambaGen Attr(HAttr attr) { return new HElemLambaGen { Name = Name, Attrs = Attrs.Prepend(attr), Kids = Kids }; }
+		public static HElemLambaGen Create(string name) { return new HElemLambaGen { Name = name }; }
+		public HElem Finish() { return new HElem(Name, Attrs.ToArray(), Kids.ToArray()); }
+	}
+
+
+
+}
