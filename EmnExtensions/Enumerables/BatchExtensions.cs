d1061e76f92b1763c630e389fe43b246afda3c13
Eamon Nerbonne <eamon@nerbonne.org>
2013-02-07 14:06:33 +0100

    Added BatchExtension: allows batching an enumerable into arrays.

---
+++ EmnExtensions/Enumerables/BatchExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmnExtensions.Enumerables {
+	public static class BatchExtension {
+		static IEnumerable<T[]> Batch<T>(this IEnumerable<T> list, int batchSize) {
+			int i = 0;
+			var arr = new T[batchSize];
+			foreach (var t in list) {
+				arr[i++] = t;
+				if (i == batchSize) {
+					yield return arr;
+					i = 0;
+					arr = new T[batchSize];
+				}
+			}
+			if (i > 0) {
+				Array.Resize(ref arr, i);
+				yield return arr;
+			}
+		}
+	}
+}
