986e5fef6f3cf8e4645329e71e821cfb210dcf09
Eamon Nerbonne <eamon@nerbonne.org>
2013-02-07 14:05:55 +0100

    Move some of the nice handy but unnecessary helpers from Fasta to EmnExtensions.

--- EmnExtensions/Filesystem/GetLinesExtension.cs
+++ EmnExtensions/Filesystem/GetLinesExtension.cs
@@ -2,21 +2,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace EmnExtensions.Filesystem
-{
-	public static class GetLinesExtension
-	{
-		public static IEnumerable<string> GetLines(this FileInfo fi) {
-			var stream = fi.OpenText();
-			while(!stream.EndOfStream)
-				yield return stream.ReadLine();
+namespace EmnExtensions.Filesystem {
+	public static class GetLinesExtension {
+		public static IEnumerable<string> Lines(this TextReader inp) {
+			using (inp)
+				for (var line = inp.ReadLine(); line != null; line = inp.ReadLine())
+					yield return line;
 		}
-		public static IEnumerable<string> GetLines(this FileInfo fi, Encoding encoding) {
-			using(Stream stream = fi.OpenRead()) {
-				var reader = new StreamReader(stream, encoding);
-				while(!reader.EndOfStream)
-					yield return reader.ReadLine();
-			}
+
+		public static IEnumerable<string> GetLines(this FileInfo fi)
+		{
+			return fi.OpenText().Lines();
+		}
+		public static IEnumerable<string> GetLines(this FileInfo fi, Encoding encoding)
+		{
+			return new StreamReader(fi.FullName, encoding).Lines();
 		}
 	}
 }
