5e24db715cd34a4f397835eae1141ece990dacbc
Eamon Nerbonne <eamon@nerbonne.org>
2013-02-11 21:18:48 +0100

    dominion - card to inlay js (1)

---
+++ dominion/CardTemplates/Scripts/ShowSets.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="knockout-2.2.1.debug.js" />
+/// <reference path="DominionSets.js" />
+
+Array.prototype.bind = function (mapper) {
+	var ret = [], tmp;
+	for (var i = 0; i < this.length; i++) {
+		tmp = mapper(this[i]);
+		for (var j = 0; j < tmp.length; j++)
+			ret.push(tmp[j]);
+	}
+	return ret;
+};
+
+var cards = DominionSets.bind(function (a) {
+	return a.Cards.map(function (c) {
+		c.SetName = a.Name;
+		return c;
+	});
+});
+cards.sort(function (a, b) { return a.Name.English.localeCompare(b.Name.English); });
+
+ko.applyBindings({ cards: cards });
