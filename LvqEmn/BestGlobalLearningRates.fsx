3eedee0451014b55ed0a889aac8ce600a2c99369
Eamon Nerbonne <eamon@nerbonne.org>
2013-04-08 23:14:44 +0200

    Update BestGlobalLearningRates script

--- LvqEmn/BestGlobalLearningRates.fsx
+++ LvqEmn/BestGlobalLearningRates.fsx
@@ -13,12 +13,12 @@ open OptimalLrSearch
 
 let defaultStore = "uniform-results.txt"
 allUniformResults defaultStore
-    |> List.rev
-    |> Seq.distinctBy (fun res-> res.Settings.WithDefaultLr()) |> Seq.toList
     |> List.sortBy (fun res->res.GeoMean)
+    |> Seq.distinctBy (fun res-> res.Settings.WithCanonicalizedDefaults()) |> Seq.toList
     //|> List.filter (fun res->res.Settings.ModelType = LvqModelType.G2m)
     |> List.map printMeanResults
     |> List.iter (printfn "%s")
+    
     // |> List.iter (fun line -> File.AppendAllText (LrOptimizer.resultsDir.FullName + "\\uniform-results-orig.txt",line + "\n"))
 
 
