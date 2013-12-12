a4e94db2865bbc859282f6410201bfdf44cbaf1c
Eamon Nerbonne <eamon@nerbonne.org>
2013-12-12 16:45:57 +0100

    Add NumericPerfTestLanguageComparison project

---
+++ Experiments/NumericPerfTestLanguageComparison/FindPrimes.fsx
@@ -0,0 +1,19 @@
+﻿#r @"packages\FSharpx.Core.1.8.28\lib\40\FSharpx.Core.dll"
+open FSharpx.Collections
+open System.Collections.Generic
+#time
+
+let findPrimes =
+    let mutablePrimes = new List<int>()
+    let rec findPrimes next =
+        seq {
+            if not <| Seq.exists (fun p -> next % p = 0) mutablePrimes then
+                yield next
+                mutablePrimes.Add next
+            yield! findPrimes (next + 1)
+        }
+    findPrimes 2 |> LazyList.ofSeq
+
+findPrimes 
+    //|> LazyList.map (fun p -> printfn "%d" p; p) 
+    |> Seq.takeWhile (fun p -> p < 300000) |> Seq.toList |> List.rev |> List.head
---
+++ Experiments/NumericPerfTestLanguageComparison/FsxNugetHack/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FsxNugetHack
+
+type Class1() = 
+    member this.X = "F#"
