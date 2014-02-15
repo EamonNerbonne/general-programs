25eaccc525669c1ad9e831f37f5886a9f365328a
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-15 08:21:30 +0100

    Make Levenshtein follow a little more normal coding conventions

--- EmnExtensions/Algorithms/Levenshtein.cs
+++ EmnExtensions/Algorithms/Levenshtein.cs
@@ -7,28 +7,18 @@ namespace EmnExtensions.Algorithms {
 	public static class Levenshtein {
 		//modified from:http://www.merriampark.com/ldcsharp.htm
 		public static int LevenshteinDistance(this string s, string t) {
-			int n = s.Length; //length of s
-			int m = t.Length; //length of t
-			int[,] d = new int[n + 1, m + 1]; // matrix
-			int cost; // cost
-			// Step 1
-			if (n == 0) return m;
-			if (m == 0) return n;
-			// Step 2
-			for (int i = 0; i <= n; d[i, 0] = i++) ;
-			for (int j = 0; j <= m; d[0, j] = j++) ;
-			// Step 3
-			for (int i = 0; i < n; i++) {
-				//Step 4
-				for (int j = 0; j < m; j++) {
-					// Step 5
-					cost = (t[j] == s[i] ? 0 : 2);//substitution will be cost 2.
-					// Step 6
-					d[i + 1, j + 1] = System.Math.Min(System.Math.Min(d[i, j + 1] + 1, d[i + 1, j] + 1), d[i, j] + cost);
+			int sLen = s.Length; //length of s
+			int tLen = t.Length; //length of t
+			int[,] d = new int[sLen + 1, tLen + 1]; // matrix
+			for (int i = 0; i <= sLen; i++) d[i, 0] = i;
+			for (int j = 0; j <= tLen; j++) d[0, j] = j;
+			for (int i = 0; i < sLen; i++) {
+				for (int j = 0; j < tLen; j++) {
+					var cost = (t[j] == s[i] ? 0 : 2);//substitution will be cost 2.
+					d[i + 1, j + 1] = Math.Min(Math.Min(d[i, j + 1] + 1, d[i + 1, j] + 1), d[i, j] + cost);
 				}
 			}
-			// Step 7
-			return d[n, m];
+			return d[sLen, tLen];
 		}
 		public static double LevenshteinDistanceScaled(this string s, string t) {
 			return LevenshteinDistance(s, t) / (double)Math.Max(1, Math.Max(s.Length, t.Length));
