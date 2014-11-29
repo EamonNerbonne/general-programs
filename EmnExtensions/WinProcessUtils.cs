ef83dd6331cb3cf7ae21be9d8221efbc8f218629
Eamon Nerbonne <eamon@nerbonne.org>
2014-11-29 21:26:33 +0100

    Add support for alternate encodings of standard out to WinProcessUtil

--- EmnExtensions/WinProcessUtils.cs
+++ EmnExtensions/WinProcessUtils.cs
@@ -1,45 +1,66 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Threading;
 
 namespace EmnExtensions {
-	/// <summary>
-	/// Useful in LINQpad queries
-	/// </summary>
-	public static class WinProcessUtil {
-		public struct ExecutionResult {
-			public string StandardOutputContents, StandardErrorContents;
-			public int ExitCode;
-		}
-		public static ExecutionResult ExecuteProcessSynchronously(string filename, string arguments, string input, ProcessPriorityClass? priority=null) {
-			using (
-				var proc = Process.Start(
-					new ProcessStartInfo {
-						CreateNoWindow = true,//don't need UI.
-						RedirectStandardOutput = true,
-						RedirectStandardError = true,
-						RedirectStandardInput = true, //so we can capture and control the new process's standard I/O
-						UseShellExecute = false,//required to be able to redirect streams
-						FileName = filename,
-						Arguments = arguments,
-					}
-				)) {
-				if(priority !=null)
-					proc.PriorityClass = priority.Value;
-				StringBuilder error = new StringBuilder(), output = new StringBuilder();
-				Thread.MemoryBarrier();
-				proc.ErrorDataReceived += (s, e) => error.Append(e.Data);
-				proc.OutputDataReceived += (s, e) => output.Append(e.Data);
-				proc.BeginErrorReadLine();
-				proc.BeginOutputReadLine();
-				if (input != null)
-					proc.StandardInput.Write(input);
-				proc.StandardInput.Close();
-				proc.WaitForExit();
-				Thread.MemoryBarrier();
-				return new ExecutionResult { StandardOutputContents = output.ToString(), StandardErrorContents = error.ToString(), ExitCode = proc.ExitCode };
-			}
-		}
-	}
+    public struct ProcessExecutionResult {
+        public string StandardOutputContents, StandardErrorContents;
+        public int ExitCode;
+    }
+
+    public struct ProcessStartOptions {
+        public ProcessPriorityClass? Priority;
+        public Encoding StandardInputEncoding;
+        public Encoding StandardOutputAndErrorEncoding;
+        public Encoding StandardErrorOverrideEncoding;
+
+    }
+    /// <summary>
+    /// Useful in LINQpad queries
+    /// </summary>
+    public static class WinProcessUtil {
+
+        public static ProcessExecutionResult ExecuteProcessSynchronously(string filename, string arguments, string input, ProcessStartOptions startOptions = default(ProcessStartOptions)) {
+            var processStartInfo = new ProcessStartInfo {
+                CreateNoWindow = true,//don't need UI.
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true, //so we can capture and control the new process's standard I/O
+                UseShellExecute = false,//required to be able to redirect streams
+                FileName = filename,
+                Arguments = arguments,
+            };
+            if (startOptions.StandardOutputAndErrorEncoding != null) {
+                processStartInfo.StandardOutputEncoding = startOptions.StandardOutputAndErrorEncoding;
+            }
+            if (startOptions.StandardErrorOverrideEncoding != null) {
+                processStartInfo.StandardErrorEncoding = startOptions.StandardErrorOverrideEncoding;
+            }
+
+            using (
+                var proc = Process.Start(
+                    processStartInfo
+                )) {
+                if (startOptions.Priority != null)
+                    proc.PriorityClass = startOptions.Priority.Value;
+                StringBuilder error = new StringBuilder(), output = new StringBuilder();
+                Thread.MemoryBarrier();
+                proc.ErrorDataReceived += (s, e) => error.Append(e.Data);
+                proc.OutputDataReceived += (s, e) => output.Append(e.Data);
+                proc.BeginErrorReadLine();
+                proc.BeginOutputReadLine();
+                using (var inputStream =
+                    startOptions.StandardInputEncoding != null
+                        ? new StreamWriter(proc.StandardInput.BaseStream, startOptions.StandardInputEncoding)
+                        : proc.StandardInput)
+                    if (input != null)
+                        inputStream.Write(input);
+                proc.WaitForExit();
+                Thread.MemoryBarrier();
+                return new ProcessExecutionResult { StandardOutputContents = output.ToString(), StandardErrorContents = error.ToString(), ExitCode = proc.ExitCode };
+            }
+        }
+    }
 }
 
