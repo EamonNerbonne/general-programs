7c8e9138cf6918149834c7392c000420155b3547
Eamon Nerbonne <eamon@nerbonne.org>
2014-12-04 22:34:05 +0100

    mingw don't parallel make to make debugging easier for now

--- LvqEmn/allconfigs/build[Mingw]Helper.bat
+++ LvqEmn/allconfigs/build[Mingw]Helper.bat
@@ -1,5 +1,7 @@
 REM @echo off
-mingw32-make "MAKE=mingw32-make -j8" SHELL=cmd.exe -j8 >%~dp0\_Build-mingw.log
+REM mingw32-make "MAKE=mingw32-make -j8" SHELL=cmd.exe -j8 >%~dp0\_Build-mingw.log
+
+mingw32-make "MAKE=mingw32-make" SHELL=cmd.exe  >%~dp0\_Build-mingw.log
 IF ERRORLEVEL 1 (
 	echo buildFailed > "%~dp0\_BuildFailed-mingw"
 ) ELSE (
