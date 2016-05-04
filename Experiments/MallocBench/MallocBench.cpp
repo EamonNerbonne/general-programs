576d2abe21121b0109bad759527e04fff1f01585
Eamon Nerbonne <eamon@nerbonne.org>
2016-05-04 10:50:57 +0200

    Update mallocbench to modern tooling

--- Experiments/MallocBench/MallocBench.cpp
+++ Experiments/MallocBench/MallocBench.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 #include <bench\BenchTimer.h>
 
-#define ITERS 1000000
+#define ITERS 10000000
+#define clocks_per_s 4000000000.0
 void main() {
 
 	Eigen::BenchTimer t;
 	for(int j=0;j<10;j++) {
 		t.start();
 		for(int i=0;i<ITERS;i++) {
-			free(malloc(128));
+			free(malloc(1));
 		}
 		t.stop();
 	}
-	std::cout <<" Best:" << t.best()<<"s;"<<t.worst()<< "s for "<<ITERS<<" iters\n";
+	std::cout <<" Best:" << t.best()/ ITERS*clocks_per_s <<"s;"<<t.worst()/ITERS*clocks_per_s << "s for "<<ITERS<<" iters\n";
 }
