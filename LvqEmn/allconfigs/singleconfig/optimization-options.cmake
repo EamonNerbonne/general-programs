3690bf19f37a5178af59e985bba101d60f5e820b
Eamon Nerbonne <eamon@nerbonne.org>
2021-04-10 21:22:53 +0200

    cleanup files

--- LvqEmn/allconfigs/singleconfig/optimization-options.cmake
+++ LvqEmn/allconfigs/singleconfig/optimization-options.cmake
@@ -4,13 +4,13 @@ ENDIF(NOT CMAKE_CONFIGURATION_TYPES AND NOT CMAKE_BUILD_TYPE)
 
 message("Using build type ${CMAKE_BUILD_TYPE}")
 if(CMAKE_COMPILER_IS_GNUCXX)
-	message("Compiler is Gcc")
+  message("Compiler is Gcc")
   
-    set(CPU_ARCH "native")
-	set(CMAKE_CXX_FLAGS_DEBUG " -Wall -fabi-version=8 -Wno-unused-local-typedefs -Wno-parentheses -std=c++11 -m64 -march=${CPU_ARCH} -mtune=native -O2")
-	set(CMAKE_CXX_FLAGS_RELEASE " -Wall -fabi-version=8 -Wno-unused-local-typedefs -Wno-parentheses -std=c++11  -m64 -march=${CPU_ARCH} -mtune=native -O3 ")
-	
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG")
+  set(CPU_ARCH "native")
+  set(CMAKE_CXX_FLAGS_DEBUG " -Wall -fabi-version=8 -Wno-unused-local-typedefs -Wno-parentheses -std=c++11 -m64 -march=${CPU_ARCH} -mtune=native -O2")
+  set(CMAKE_CXX_FLAGS_RELEASE " -Wall -fabi-version=8 -Wno-unused-local-typedefs -Wno-parentheses -std=c++11  -m64 -march=${CPU_ARCH} -mtune=native -O3 ")
+
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG")
 
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -ftree-loop-distribution -ftree-loop-im -ftree-loop-ivcanon -fivopts")#*slightly* good for build3v
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fassociative-math -fno-trapping-math -fno-signed-zeros -fno-math-errno")#-ffinite-math-only  -funsafe-math-optimizations -ffast-math
@@ -26,23 +26,23 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   #set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wsuggest-attribute=const")
   #set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -flto ")#-fwhole-program
 
-#-ftree-parallelize-loops=4 -floop-interchange 
+  #-ftree-parallelize-loops=4 -floop-interchange 
   #set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wunsafe-loop-optimizations")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -funsafe-loop-optimizations ")#bad for build3
-	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -ftracer") #bad for build3
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -funsafe-loop-optimizations ")#bad for build3
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -ftracer") #bad for build3
 
-	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s")
-	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -s ")
-	set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -s ")
-	set(CMAKE_LIBRARY_LINKER_FLAGS "${CMAKE_LIBRARY_LINKER_FLAGS} -s ")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s")
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -s ")
+  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -s ")
+  set(CMAKE_LIBRARY_LINKER_FLAGS "${CMAKE_LIBRARY_LINKER_FLAGS} -s ")
 elseif(MSVC)
-	message("Compiler is msvc")
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHa /arch:SSE2 /W4") #SEH exceptions,SSE2 support, extra warnings
-	set(CMAKE_CXX_FLAGS_RELEASE " /Ox /Ot /GL /Oy /Oi /GS- /fp:fast /fp:except- /MD -DNDEBUG")
-	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /LTCG /OPT:REF /OPT:ICF /MACHINE:X64 /INCREMENTAL:NO")
-	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /LTCG /OPT:REF /OPT:ICF /MACHINE:X64  /INCREMENTAL:NO")
-	set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS}  /LTCG /OPT:REF /OPT:ICF /MACHINE:X64  /INCREMENTAL:NO")
-	set(CMAKE_LIBRARY_LINKER_FLAGS "${CMAKE_LIBRARY_LINKER_FLAGS}  /LTCG /OPT:REF /OPT:ICF /MACHINE:X64  /INCREMENTAL:NO")
+  message("Compiler is msvc")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHa /arch:SSE2 /W4") #SEH exceptions,SSE2 support, extra warnings
+  set(CMAKE_CXX_FLAGS_RELEASE " /Ox /Ot /GL /Oy /Oi /GS- /fp:fast /fp:except- /MD -DNDEBUG")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /LTCG /OPT:REF /OPT:ICF /MACHINE:X64 /INCREMENTAL:NO")
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /LTCG /OPT:REF /OPT:ICF /MACHINE:X64  /INCREMENTAL:NO")
+  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS}  /LTCG /OPT:REF /OPT:ICF /MACHINE:X64  /INCREMENTAL:NO")
+  set(CMAKE_LIBRARY_LINKER_FLAGS "${CMAKE_LIBRARY_LINKER_FLAGS}  /LTCG /OPT:REF /OPT:ICF /MACHINE:X64  /INCREMENTAL:NO")
 else()
-	message(FATAL_ERROR "Unrecognized compiler!")
+  message(FATAL_ERROR "Unrecognized compiler!")
 endif()
