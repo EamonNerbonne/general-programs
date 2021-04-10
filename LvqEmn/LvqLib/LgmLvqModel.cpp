3ea9b7b222537d30af3127eacc713eaee903c5bb
Eamon Nerbonne <eamon@nerbonne.org>
2021-04-10 17:45:05 +0200

    Try to differentiate between clang, gcc, and msc related hacks, and get this thing to actually work on cl.exe (msc)

--- LvqEmn/LvqLib/LgmLvqModel.cpp
+++ LvqEmn/LvqLib/LgmLvqModel.cpp
@@ -1,4 +1,4 @@
-#include "StdAfx.h"
+#include "stdafx.h"
 #include "LgmLvqModel.h"
 #include "utils.h"
 #include "LvqConstants.h"
--- LvqEmn/LvqLib/stdafx.h
+++ LvqEmn/LvqLib/stdafx.h
@@ -1,8 +1,4 @@
 #pragma once
-#ifdef _MSC_VER
-#endif
-
-
 
 //#define EIGEN_DONT_VECTORIZE
 //#define EIGEN_VECTORIZE_SSE3
@@ -28,16 +24,22 @@
 #pragma warning(disable: 4610) //OK to not create default constructor
 #pragma warning(disable: 4701) //too many false positives for uninitalized locals
 #pragma warning(disable: 4800)//eigen internal perf warning
+
+#pragma warning(disable: 4702) //unreachable code in eigen?  probably some kind of template specialization noise
+#pragma warning(disable: 4127) //"conditional expression is constant" - tons of those in eigen
 #endif
 
 #include <Eigen/Core>
 #include <Eigen/LU> 
 #include <Eigen/QR> 
 #include <Eigen/StdVector>
+#pragma warning(disable: 4100) //unreferenced formal parameter
+#pragma warning(disable: 4505) //unreferenced formal parameter
 #include <bench/BenchTimer.h>
 
 #ifdef _MSC_VER
 #pragma warning(pop)
+#pragma warning(disable: 4127) //"conditional expression is constant" - tons of those in eigen
 #endif
 
 
